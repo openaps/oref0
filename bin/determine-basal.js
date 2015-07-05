bf489555c4c416be060ba78581488f86f6c75fd6
Scott Leibrand <scottleibrand@gmail.com>
2015-07-05 01:25:59 -0700

    check whether temps_data.duration > 0 to differentiate no-temp from zero-temp

--- bin/determine-basal.js
+++ bin/determine-basal.js
@@ -108,7 +108,7 @@ if (!module.parent) {
                 // rate required to deliver insulinReq less insulin over 30m:
                 var rate = profile_data.current_basal - (2 * insulinReq);
 
-                if (typeof temps_data.rate === 'undefined' || rate < temps_data.rate) { // if required temp < existing temp basal
+                if (typeof temps_data.rate === 'undefined' || ( temps_data.duration > 0 && rate < temps_data.rate )) { // if required temp < existing temp basal
                     setTempBasal(rate, 30);
                 } else {
                     console.error("No action required (existing basal " + temps_data.rate + " <= required temp " + rate + " )")
