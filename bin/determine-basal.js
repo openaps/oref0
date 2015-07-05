944d9ee044050b3ca494cec63bfc2113c3f6aee3
Scott Leibrand <scottleibrand@gmail.com>
2015-07-05 02:16:09 -0700

    don't reissue temp if existing temp is within 0.1U of required

--- bin/determine-basal.js
+++ bin/determine-basal.js
@@ -108,8 +108,8 @@ if (!module.parent) {
                 // rate required to deliver insulinReq less insulin over 30m:
                 var rate = profile_data.current_basal - (2 * insulinReq);
 
-                if (typeof temps_data.rate !== 'undefined' && ( temps_data.duration > 0 && rate <= temps_data.rate )) { // if required temp < existing temp basal
-                    console.error("No action required (existing basal " + temps_data.rate + " <= required temp " + rate + " )")
+                if (typeof temps_data.rate !== 'undefined' && ( temps_data.duration > 0 && rate - 0.1 <= temps_data.rate )) { // if required temp < existing temp basal
+                    console.error("No action required (existing basal " + temps_data.rate + " <~ required temp " + rate + " )")
                 } else {
                     setTempBasal(rate, 30);
                 }
@@ -120,10 +120,10 @@ if (!module.parent) {
                 var insulinReq = (target_bg - eventualBG) / profile_data.sens;
                 // rate required to deliver insulinReq more insulin over 30m:
                 var rate = profile_data.current_basal - (2 * insulinReq);
-                if (typeof temps_data.rate === 'undefined' || rate > temps_data.rate){ // if required temp > existing temp basal
-                    setTempBasal(rate, 30);
+                if (typeof temps_data.rate !== 'undefined' && rate + 0.1 > temps_data.rate){ // if required temp > existing temp basal
+                    console.error("No action required (existing basal " + temps_data.rate + " >~ required temp " + rate + " )")
                 } else {
-                    console.error("No action required (existing basal " + temps_data.rate + " >= required temp " + rate + " )")
+                    setTempBasal(rate, 30);
                 }
         
             } else { // if (profile_data.min_bg < eventualBG < profile_data.max_bg) {
