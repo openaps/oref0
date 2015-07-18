e09631bbe5229cd12ff8aadf94293b7bac03e161
Scott Leibrand <scottleibrand@gmail.com>
2015-07-18 12:07:37 -0700

    replace calculate-iob and determine-basal with their openaps invoke equivalents

--- bin/pebble.sh
+++ bin/pebble.sh
@@ -7,8 +7,11 @@ git pull
 stat -c %y clock.json | cut -c 1-19
 cat clock.json | sed 's/"//g' | sed 's/T/ /'
 echo
-calculate-iob pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && mv iob.json.new iob.json
-determine-basal iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep temp requestedtemp.json.new && mv requestedtemp.json.new requestedtemp.json
+#calculate-iob pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && mv iob.json.new iob.json
+openaps invoke iob.json.new && grep iob iob.json.new && mv iob.json.new iob.json
+#determine-basal iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep temp requestedtemp.json.new && mv requestedtemp.json.new requestedtemp.json
+openaps invoke requestedtemp.json.new && grep temp requestedtemp.json.new && mv requestedtemp.json.new requestedtemp.json
+git pull && git push
 node ~/openaps-js/bin/pebble.js  glucose.json clock.json iob.json current_basal_profile.json currenttemp.json isf.json requestedtemp.json > /tmp/pebble-openaps.json
 cat /tmp/pebble-openaps.json
 jsonlint /tmp/pebble-openaps.json && cp /tmp/pebble-openaps.json /var/www/openaps.json 
