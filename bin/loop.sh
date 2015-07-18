0cf402e0057e4fc1daf7653d1babdcecd3461c50
Scott Leibrand <scottleibrand@gmail.com>
2015-07-18 16:39:05 -0700

    let's try cp instead of mv

--- bin/loop.sh
+++ bin/loop.sh
@@ -21,7 +21,7 @@ git fetch --all && git reset --hard origin/master && git pull
 
 echo "Querying CGM"
 openaps report invoke glucose.json.new || openaps report invoke glucose.json.new 
-grep glucose glucose.json.new && mv glucose.json.new glucose.json && git commit -m"glucose.json has glucose data: committing" glucose.json
+grep glucose glucose.json.new && cp glucose.json.new glucose.json && git commit -m"glucose.json has glucose data: committing" glucose.json
 git pull && git push
 #grep glucose glucose.json || git reset --hard origin/master
 find glucose.json -mmin 3 && grep glucose glucose.json || die "Can't read from CGM"
@@ -36,20 +36,20 @@ fi
 
 echo "Checking pump status"
 openaps status || openaps status || die "Can't get pump status"
-grep status status.json.new && mv status.json.new status.json
+grep status status.json.new && cp status.json.new status.json
 git pull && git push
 echo "Querying pump"
 #openaps pumpquery || openaps pumpquery || die "Can't query pump" && git pull && git push
 openaps pumpquery || openaps pumpquery
-grep T clock.json.new && mv clock.json.new clock.json
-grep temp currenttemp.json.new && mv currenttemp.json.new currenttemp.json
-grep timestamp pumphistory.json.new && mv pumphistory.json.new pumphistory.json
+grep T clock.json.new && cp clock.json.new clock.json
+grep temp currenttemp.json.new && cp currenttemp.json.new currenttemp.json
+grep timestamp pumphistory.json.new && cp pumphistory.json.new pumphistory.json
 git pull && git push
 
 openaps suggest
-grep sens profile.json.new && mv profile.json.new profile.json
-grep iob iob.json.new && mv iob.json.new iob.json
-grep temp requestedtemp.json.new && mv requestedtemp.json.new requestedtemp.json
+grep sens profile.json.new && cp profile.json.new profile.json
+grep iob iob.json.new && cp iob.json.new iob.json
+grep temp requestedtemp.json.new && cp requestedtemp.json.new requestedtemp.json
 git pull && git push
 
 tail clock.json
