ab5e05597bce57fba23ee53f50938075ae2196fc
Scott Leibrand <scottleibrand@gmail.com>
2015-07-20 09:18:00 -0700

    git merge -X theirs

--- bin/pebble.sh
+++ bin/pebble.sh
@@ -9,7 +9,8 @@ echo
 node ~/openaps-js/bin/iob.js pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && cp iob.json.new iob.json && git commit -m"iob found: committing" iob.json
 #determine-basal iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep temp requestedtemp.json.new && mv requestedtemp.json.new requestedtemp.json
 node ~/openaps-js/bin/determine-basal.js iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep temp requestedtemp.json.new && cp requestedtemp.json.new requestedtemp.json && git commit -m"temp found: committing" requestedtemp.json
-git pull && git push
+git fetch origin master && git merge -X theirs origin/master && git push
+#git pull && git push
 node ~/openaps-js/bin/pebble.js  glucose.json clock.json iob.json current_basal_profile.json currenttemp.json isf.json requestedtemp.json > /tmp/pebble-openaps.json
 cat /tmp/pebble-openaps.json
 jsonlint /tmp/pebble-openaps.json && cp /tmp/pebble-openaps.json /var/www/openaps.json 
