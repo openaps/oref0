3cf81ef7fb6d622773d511aa8da6e7cd031b86e5
Scott Leibrand <scottleibrand@gmail.com>
2015-07-25 00:23:43 -0700

    update /etc/init.d/hostapd

--- headless/install.sh
+++ headless/install.sh
@@ -5,5 +5,6 @@ mv /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
 cp ./dnsmasq.conf /etc/dnsmasq.conf
 mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
 cp ./hostapd.conf /etc/hostapd/hostapd.conf
+sed -i.bak -e "s|DAEMON_CONF=|DAEMON_CONF=/etc/hostapd/hostapd.conf|g" /etc/init.d/hostapd
 cp interfaces.* /etc/network/
-ls $PWD/headless.sh && ( grep headless /etc/crontab || echo "* * * * * root $PWD/headless.sh" ) >> /etc/crontab
+ls $PWD/headless.sh && ( grep -q headless /etc/crontab || echo "* * * * * root $PWD/headless.sh" ) >> /etc/crontab
