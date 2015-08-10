06cd3e3a7ff229da66972d450520d264eb661102
Scott Leibrand <scottleibrand@gmail.com>
2015-08-10 06:36:07 -0700

    just match on durations, not rates, to avoid overriding rate=0

--- bin/determine-basal.js
+++ bin/determine-basal.js
@@ -225,7 +225,7 @@ if (!module.parent) {
     }
 
 // if no temp is running or required, set the current basal as a temp, so you can see on the pump that the loop is working
-if ((!temps_data.rate || !temps_data.duration) && !requestedTemp.rate) {
+if (!temps_data.duration && !requestedTemp.duration) {
     setTempBasal(profile_data.current_basal, 30);
     reason = reason + "; setting current basal of " + profile_data.current_basal + " as temp";
 }
