5f4120414035d5bf63dde41fdade22bd967fc90a
Scott Leibrand <scottleibrand@gmail.com>
2015-08-14 07:30:44 -0700

    only use fake-hwclock if ntp-wait fails

--- bin/clockset.sh
+++ bin/clockset.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+die() { echo "$@" ; exit 1; }
+
+ntp-wait -n 1 -v && die "NTP already synchronized."
+
 cd ~/openaps-dev
 ( cat clock.json; echo ) | sed 's/"//g' | sed "s/$/`date +%z`/" | while read line; do date -u -d $line +"%F %R:%S"; done > fake-hwclock.data
 sudo cp fake-hwclock.data /etc/fake-hwclock.data
@@ -8,4 +12,3 @@ sudo fake-hwclock load
 grep display_time glucose.json | head -1 | awk '{print $2}' | sed "s/,//" | sed 's/"//g' | sed "s/$/`date +%z`/" | while read line; do date -u -d $line +"%F %R:%S"; done > fake-hwclock.data
 sudo cp fake-hwclock.data /etc/fake-hwclock.data
 sudo fake-hwclock load
-ntp-wait -n 1 -v && echo true || echo false
