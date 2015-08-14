06297ac666ab2adb002cbf2c28ec43cdbd96c5bd
Scott Leibrand <scottleibrand@gmail.com>
2015-08-14 07:47:42 -0700

    add support for share2 glucose data as well, and add check for data before using

--- bin/clockset.sh
+++ bin/clockset.sh
@@ -7,8 +7,9 @@ ntp-wait -n 1 -v && die "NTP already synchronized."
 
 cd ~/openaps-dev
 ( cat clock.json; echo ) | sed 's/"//g' | sed "s/$/`date +%z`/" | while read line; do date -u -d $line +"%F %R:%S"; done > fake-hwclock.data
-sudo cp fake-hwclock.data /etc/fake-hwclock.data
+grep : fake-hwclock.data && sudo cp fake-hwclock.data /etc/fake-hwclock.data
 sudo fake-hwclock load
-grep display_time glucose.json | head -1 | awk '{print $2}' | sed "s/,//" | sed 's/"//g' | sed "s/$/`date +%z`/" | while read line; do date -u -d $line +"%F %R:%S"; done > fake-hwclock.data
-sudo cp fake-hwclock.data /etc/fake-hwclock.data
+grep -q display_time glucose.json && grep display_time glucose.json | head -1 | awk '{print $2}' | sed "s/,//" | sed 's/"//g' | sed "s/$/`date +%z`/" | while read line; do date -u -d $line +"%F %R:%S"; done > fake-hwclock.data
+grep -q dateString glucose.json && grep dateString glucose.json | head -1 | awk '{print $2}' | sed "s/,//" | sed 's/"//g' |while read line; do date -u -d $line +"%F %R:%S"; done > fake-hwclock.data
+grep : fake-hwclock.data && sudo cp fake-hwclock.data /etc/fake-hwclock.data
 sudo fake-hwclock load
