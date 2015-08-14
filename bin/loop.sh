afe3049d2775e9c86364bddfe399d6e9679dec05
Scott Leibrand <scottleibrand@gmail.com>
2015-08-14 05:46:34 -0700

    break out pumpsettings.sh from loop.sh

--- bin/loop.sh
+++ bin/loop.sh
@@ -56,7 +56,7 @@ tail clock.json
 tail currenttemp.json
 
 rm requestedtemp.json*
-openaps suggest || die "Can't calculate IOB or basal"
+openaps suggest || ( ~/openaps-js/bin/pumpsettings.sh && openaps suggest ) || die "Can't calculate IOB or basal"
 find clock.json -mmin -10 | egrep -q '.*' && find glucose.json -mmin -10 | egrep -q '.*' && find pumphistory.json -mmin -10 | egrep -q '.*' && find requestedtemp.json -mmin -10 | egrep -q '.*' && ~/openaps-js/bin/pebble.sh
 tail profile.json
 tail iob.json
@@ -76,19 +76,4 @@ find clock.json -mmin -10 | egrep -q '.*' && ~/bin/openaps-mongo.sh
 
 ls /tmp/openaps.lock >/dev/null 2>/dev/null && die "OpenAPS already running: exiting" && exit
 touch /tmp/openaps.lock
-echo "Querying pump settings"
-openaps pumpsettings || openaps pumpsettings # || die "Can't query pump settings" # && git pull && git push
-grep -q '"start": "00:00:00",' carb_ratio.json.new || die "Couldn't find first carb ratio schedule entry: bailing"
-grep -q '"start": "00:00:00",' current_basal_profile.json.new || die "Couldn't find first basal profile schedule entry: bailing"
-grep -q '"start": "00:00:00",' isf.json.new || die "Couldn't find first ISF schedule entry: bailing"
-grep -q '"start": "00:00:00",' bg_targets.json.new || die "Couldn't find first BG targets schedule entry: bailing"
-grep -q '"sensitivity": 0,' isf.json.new && die "Sensitivity of 0 makes no sense: bailing"
-grep -q '"units": null,' carb_ratio.json.new && die "null units for carb ratio: bailing"
-grep -q '"rate": 0.0' current_basal_profile.json.new && die "basal rates < 0.1U/hr not supported: bailing"
-grep -q '"insulin_action_curve": 0' pump_settings.json.new && die "DIA of 0 makes no sense: bailing"
-grep -q insulin_action_curve pump_settings.json.new && cp pump_settings.json.new pump_settings.json
-grep -q "mg/dL" bg_targets.json.new && cp bg_targets.json.new bg_targets.json
-grep -q sensitivity isf.json.new && cp isf.json.new isf.json
-grep -q rate current_basal_profile.json.new && cp current_basal_profile.json.new current_basal_profile.json
-grep -q grams carb_ratio.json.new && cp carb_ratio.json.new carb_ratio.json
-
+~/openaps-js/bin/pumpsettings.sh
