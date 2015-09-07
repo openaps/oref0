96fce730f7f0611d6227be899c1e019ccd55ba41
Scott Leibrand <scottleibrand@gmail.com>
2015-09-07 03:39:18 -0700

    ping google to see if we're online instead of relying on openaps-mongo.sh exit codes

--- bin/loop.sh
+++ bin/loop.sh
@@ -68,7 +68,7 @@ querypump() {
     upload
 }
 # try to upload pumphistory data
-upload() { findpumphistory && ~/bin/openaps-mongo.sh && touch /tmp/openaps.online; }
+upload() { findpumphistory && ~/bin/openaps-mongo.sh; ping -c 1 google.com > /dev/null && touch /tmp/openaps.online; }
 # if we haven't uploaded successfully in 10m, use offline mode (if no temp running, set current basal as temp to show the loop is working)
 suggest() {
     openaps suggest || echo -n "!" >> /var/log/openaps/easy.log
