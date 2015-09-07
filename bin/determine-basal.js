8e69268cb7e4b73cb942ba46bf36d1e8e93fe151
Scott Leibrand <scottleibrand@gmail.com>
2015-09-07 00:22:57 -0700

    only set the current basal as a 30m temp in Offline mode

--- bin/determine-basal.js
+++ bin/determine-basal.js
@@ -45,10 +45,12 @@ function setTempBasal(rate, duration) {
     if (rate < 0) { rate = 0; } // if >30m @ 0 required, zero temp will be extended to 30m instead
     else if (rate > maxSafeBasal) { rate = maxSafeBasal; }
     
-    // rather than canceling temps, always set the current basal as a 30m temp
+    // rather than canceling temps, if Offline mode is set, always set the current basal as a 30m temp
     // so we can see on the pump that openaps is working
     if (duration == 0) {
-        rate = profile_data.current_basal;
+        if (offline_input == 'Offline') {
+            rate = profile_data.current_basal;
+        }
         duration  = 30;
     }
 
