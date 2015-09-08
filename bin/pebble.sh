8374cba76dbf6861a64a7db2318b95f84d83b457
Scott Leibrand <scottleibrand@gmail.com>
2015-09-07 22:12:51 -0700

    rewrite and simplification of pebble code to use timestamps and file mtimes, and re-run determine-basal.js every time

--- bin/pebble.sh
+++ bin/pebble.sh
@@ -20,8 +20,9 @@ cd ~/openaps-dev
 stat -c %y clock.json | cut -c 1-19
 cat clock.json | sed 's/"//g' | sed 's/T/ /'
 echo
-node ~/openaps-js/bin/iob.js pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && cp iob.json.new iob.json #&& git commit -m"iob found: committing" iob.json
-node ~/openaps-js/bin/pebble.js  glucose.json clock.json iob.json current_basal_profile.json currenttemp.json isf.json requestedtemp.json > /tmp/pebble-openaps.json
+node ~/openaps-js/bin/iob.js pumphistory.json profile.json clock.json > iob.json.new && grep iob iob.json.new && rsync -tu iob.json.new iob.json
+node ~/openaps-js/bin/determine-basal.js iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep reason requestedtemp.json.new && rsync -tu requestedtemp.json.new requestedtemp.json
+node ~/openaps-js/bin/pebble.js glucose.json iob.json current_basal_profile.json currenttemp.json requestedtemp.json enactedtemp.json > /tmp/pebble-openaps.json
 #cat /tmp/pebble-openaps.json
 grep "refresh_frequency" /tmp/pebble-openaps.json && rsync -tu /tmp/pebble-openaps.json www/openaps.json 
 #cat www/openaps.json
