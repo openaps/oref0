95599737e7957aff7fc85636b8df506d66e27ac0
Scott Leibrand <scottleibrand@gmail.com>
2015-09-16 22:22:52 -0700

    fix iob.js for non-3h DIAs, and do bolus IOB properly

--- bin/iob.js
+++ bin/iob.js
@@ -20,7 +20,8 @@
 
 function iobCalc(treatment, time, dia) {
     var diaratio = dia / 3;
-    var peak = 75 * diaratio;
+    var peak = 75 ;
+    var end = 180 ;
     //var sens = profile_data.sens;
     if (typeof time === 'undefined') {
         var time = new Date();
@@ -28,21 +29,20 @@ function iobCalc(treatment, time, dia) {
 
     if (treatment.insulin) {
         var bolusTime=new Date(treatment.date);
-        var minAgo=(time-bolusTime)/1000/60;
+        var minAgo=(time-bolusTime)/1000/60 * diaratio;
 
         if (minAgo < 0) { 
             var iobContrib=0;
             var activityContrib=0;
         }
-        if (minAgo < peak) {
-            var x = (minAgo/5 + 1) * diaratio;
+        else if (minAgo < peak) {
+            var x = (minAgo/5 + 1);
             var iobContrib=treatment.insulin*(1-0.001852*x*x+0.001852*x);
             //var activityContrib=sens*treatment.insulin*(2/dia/60/peak)*minAgo;
             var activityContrib=treatment.insulin*(2/dia/60/peak)*minAgo;
-
         }
-        else if (minAgo < 180) {
-            var x = (minAgo-peak)/5 * diaratio;
+        else if (minAgo < end) {
+            var x = (minAgo-peak)/5;
             var iobContrib=treatment.insulin*(0.001323*x*x - .054233*x + .55556);
             //var activityContrib=sens*treatment.insulin*(2/dia/60-(minAgo-peak)*2/dia/60/(60*dia-peak));
             var activityContrib=treatment.insulin*(2/dia/60-(minAgo-peak)*2/dia/60/(60*dia-peak));
@@ -77,7 +77,7 @@ function iobTotal(treatments, time) {
             if (tIOB && tIOB.activityContrib) activity += tIOB.activityContrib;
             // keep track of bolus IOB separately for snoozes, but decay it three times as fast
             if (treatment.insulin >= 0.2 && treatment.started_at) {
-                var bIOB = iobCalc(treatment, time, dia/3)
+                var bIOB = iobCalc(treatment, time, dia*3)
                 //console.log(treatment);
                 //console.log(bIOB);
                 if (bIOB && bIOB.iobContrib) bolusiob += bIOB.iobContrib;
