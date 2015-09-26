482e303b3c3bc097ef04eac05b39de532f1a1d38
cjo20 <git@cjo20.net>
2015-09-27 00:58:12 +0100

    In ns-upload-glucose, fix 'type' for sgv entries, set device to show it comes from cgm

--- bin/ns-upload-glucose.sh
+++ bin/ns-upload-glucose.sh
@@ -10,7 +10,8 @@ cat $HISTORY | \
   json -e "this.medtronic = this._type;" | \
   json -e "this.dateString = this.date + '$(date +%z)'" | \
   json -e "this.date = new Date(this.dateString).getTime();" | \
-  json -e "this.type = 'medtronic'" \
+  json -e "this.type = (this.name == 'GlucoseSensorData') ? 'sgv' : 'pumpdata'" | \
+  json -e "this.device = 'openaps://medtronic/pump/cgm'" \
   > $OUTPUT
 
 # requires API_SECRET and NIGHTSCOUT_HOST to be set in calling environment (i.e. in crontab)
