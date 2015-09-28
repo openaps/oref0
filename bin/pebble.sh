497057e2c0c54e7a0ebaafd4cb4810921cb407f8
Rachel <audiefile@gmail.com>
2015-09-27 20:13:17 -0400

    changed so pebble card can read from apache server

--- bin/pebble.sh
+++ bin/pebble.sh
@@ -28,5 +28,5 @@ node ~/openaps-js/bin/iob.js pumphistory.json profile.json now.json > iob.json.n
 node ~/openaps-js/bin/determine-basal.js iob.json currenttemp.json glucose.json profile.json > requestedtemp.json.new && grep reason requestedtemp.json.new && rsync -tu requestedtemp.json.new requestedtemp.json
 node ~/openaps-js/bin/pebble.js glucose.json iob.json current_basal_profile.json currenttemp.json requestedtemp.json enactedtemp.json > /tmp/pebble-openaps.json
 #cat /tmp/pebble-openaps.json
-grep "refresh_frequency" /tmp/pebble-openaps.json && rsync -tu /tmp/pebble-openaps.json www/openaps.json 
+grep "refresh_frequency" /tmp/pebble-openaps.json && rsync -tu /tmp/pebble-openaps.json /var/www/openaps.json 
 #cat www/openaps.json
