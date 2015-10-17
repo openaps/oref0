7aaebd3d0b8a20a82e3dcd3ffd9bcca633c8c3cd
Ben West <bewest@gmail.com>
2015-10-17 15:38:36 -0700

    refactor, reorganize into source modules
    Make simpler modules that can be require'd, tested, and re-used with ease.

--- bin/oref0-get-profile.js
+++ bin/oref0-get-profile.js
@@ -16,90 +16,7 @@
 
 */
 
-function getTime(minutes) {
-    var baseTime = new Date();
-    baseTime.setHours('00');
-    baseTime.setMinutes('00');
-    baseTime.setSeconds('00');    
-    
-    return baseTime.getTime() + minutes * 60 * 1000;
-   
-}
-
-/* Return basal rate(U / hr) at the provided timeOfDay */
-
-function basalLookup() {
-    var now = new Date();
-    var basalRate = basalprofile_data[basalprofile_data.length-1].rate
-    
-    for (var i = 0; i < basalprofile_data.length - 1; i++) {
-        if ((now >= getTime(basalprofile_data[i].minutes)) && (now < getTime(basalprofile_data[i + 1].minutes))) {
-            basalRate = basalprofile_data[i].rate;
-            break;
-        }
-    }
-    profile.current_basal= Math.round(basalRate*1000)/1000;
-}
-
-function bgTargetsLookup(){
-    var now = new Date();
-    
-    //bgtargets_data.targets.sort(function (a, b) { return a.offset > b.offset });
-
-    var bgTargets = bgtargets_data.targets[bgtargets_data.targets.length - 1]
-    
-    for (var i = 0; i < bgtargets_data.targets.length - 1; i++) {
-        if ((now >= getTime(bgtargets_data.targets[i].offset)) && (now < getTime(bgtargets_data.targets[i + 1].offset))) {
-            bgTargets = bgtargets_data.targets[i];
-            break;
-        }
-    }
-    // hard-code lower bounds for min_bg and max_bg in case pump is set too low, or units are wrong
-    profile.max_bg = max(100,bgTargets.high);
-    profile.min_bg = max(90,bgTargets.low);
-    // hard-code upper bound for min_bg in case pump is set too high
-    profile.min_bg = min(200,profile.min_bg);
-}
-
-function carbRatioLookup() {
-    var now = new Date();
-    //carbratio_data.schedule.sort(function (a, b) { return a.offset > b.offset });
-    var carbRatio = carbratio_data.schedule[carbratio_data.schedule.length - 1]
-    
-    for (var i = 0; i < carbratio_data.schedule.length - 1; i++) {
-        if ((now >= getTime(carbratio_data.schedule[i].offset)) && (now < getTime(carbratio_data.schedule[i + 1].offset))) {
-            carbRatio = carbratio_data.schedule[i];
-            break;
-        }
-    }
-    profile.carbratio = carbRatio.ratio;    
-}
-
-function isfLookup() {
-    var now = new Date();
-    //isf_data.sensitivities.sort(function (a, b) { return a.offset > b.offset });
-    var isfSchedule = isf_data.sensitivities[isf_data.sensitivities.length - 1]
-    
-    for (var i = 0; i < isf_data.sensitivities.length - 1; i++) {
-        if ((now >= getTime(isf_data.sensitivities[i].offset)) && (now < getTime(isf_data.sensitivities[i + 1].offset))) {
-            isfSchedule = isf_data.sensitivities[i];
-            break;
-        }
-    }
-    profile.sens = isfSchedule.sensitivity;
-}
-
-function maxDailyBasal(){
-    basalprofile_data.sort(function (a, b) { if (a.rate < b.rate) { return 1 } if (a.rate > b.rate) { return -1; } return 0; });
-    profile.max_daily_basal = Math.round( basalprofile_data[0].rate *1000)/1000;
-}
-
-/*Return maximum daily basal rate(U / hr) from profile.basals */
-
-function maxBasalLookup() {
-    
-    profile.max_basal =pumpsettings_data.maxBasal;
-}
+var generate = require('oref0/lib/profile/');
 
 if (!module.parent) {
     
@@ -127,23 +44,21 @@ if (!module.parent) {
     var basalprofile_data = require(cwd + '/' + basalprofile_input);
     var carbratio_data = require(cwd + '/' + carbratio_input);;
 
-    var profile = {        
-          carbs_hr: 28 // TODO: verify this is completely unused and consider removing it if so
-        , max_iob: 0 // if max_iob.json is not profided, never give more insulin than the pump would have
-        , dia: pumpsettings_data.insulin_action_curve        
-        , type: "current"
+    var maxiob_data = { max_iob: 0 };
+    if (typeof maxiob_input != 'undefined') {
+        maxiob_data = require(cwd + '/' + maxiob_input);
+    }
+    var inputs = {
+      settings: pumpsettings_data
+    , targets: bgtargets_data
+    , basals: basalprofile_data
+    , isf: isf_data
+    , max_iob: maxiob_data.max_iob || 0
+    , carbs: carbratio_data
+
     };
 
-    basalLookup();
-    maxDailyBasal();
-    maxBasalLookup()
-    bgTargetsLookup();
-    carbRatioLookup();
-    isfLookup();
-    if (typeof maxiob_input != 'undefined') {
-        var maxiob_data = require(cwd + '/' + maxiob_input);
-        profile.max_iob = maxiob_data.max_iob;
-    }
+    var profile = generate(inputs);
 
     console.log(JSON.stringify(profile));
 }
