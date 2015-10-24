b2d1189d5e808d33b02378087fb21c800347e833
Ben West <bewest@gmail.com>
2015-10-23 18:28:32 -0700

    don't overwrite model file ;)

--- bin/mm-format-ns-treatments.sh
+++ bin/mm-format-ns-treatments.sh
@@ -4,7 +4,7 @@
 
 HISTORY=${1-monitor/pump-history-zoned.json}
 MODEL=${2-model.json}
-OUTPUT=${2-/dev/fd/1}
+OUTPUT=${3-/dev/fd/1}
 #TZ=${3-$(date +%z)}
 self=$(basename $0)
 
@@ -15,7 +15,7 @@ oref0-normalize-temps $HISTORY  \
   | json -e "this.medtronic = 'mm://openaps/$self/' + (this._type || this.eventType);" \
   | json -e "this.created_at = this.created_at ? this.created_at : this.timestamp" \
   | json -e "this.enteredBy = 'openaps://medtronic/$model'" \
-  | json -e "if (this.glucose && !this.glucoseType && this.glucose > 0) { this.glucoseType = this._type + '/$model' }" \
+  | json -e "if (this.glucose && !this.glucoseType && this.glucose > 0) { this.glucoseType = this.enteredBy }" \
   | json -e "this.eventType = (this.eventType ?  this.eventType : 'Note')" \
   | json -e "if (this.eventType == 'Note') { this.notes = this._type + ' $model ' + (this.notes ? this.notes : '')}" \
   | json > $OUTPUT
