1f44797b13fe38224cdbb0e1d7bd71d4d173487c
Jason Calabrese <jason@cbrese.com>
2015-11-08 19:36:59 -0800

    also handle the 3rd and 4th points, needs more fixing

--- bin/oref0-determine-basal.js
+++ bin/oref0-determine-basal.js
@@ -69,38 +69,40 @@ function init() {
     };
 
     determinebasal.getLastGlucose = function getLastGlucose(data) {
-        
-        var now = data[0];
-        var last = data[1];
+
+        function prepGlucose (obj) {
+          //Support the NS sgv field to avoid having to convert in a custom way
+          obj.glucose = obj.glucose || obj.sgv;
+          return obj;
+        }
+
+        var now = prepGlucose(data[0]);
+        var last = prepGlucose(data[1]);
+        var old = prepGlucose(data[2]);
+        var oldold = prepGlucose(data[3]);
         var minutes;
         var change;
         var avg;
 
-        //Support the NS sgv field to avoid having to convert in a custom way
-        now.glucose = now.glucose || now.sgv;
-        last.glucose = last.glucose || last.sgv;
-
         //TODO: calculate average using system_time instead of assuming 1 data point every 5m
-        if (typeof data[3] !== 'undefined' && data[3].glucose > 30) {
+        if (typeof oldold !== 'undefined' && oldold.glucose > 30) {
             minutes = 3*5;
-            change = now.glucose - data[3].glucose;
-        } else if (typeof data[2] !== 'undefined' && data[2].glucose > 30) {
+            change = now.glucose - oldold.glucose;
+        } else if (old !== 'undefined' && old.glucose > 30) {
             minutes = 2*5;
-            change = now.glucose - data[2].glucose;
-        } else if (typeof data[1] !== 'undefined' && data[1].glucose > 30) {
-            minutes = 1*5;
-            change = now.glucose - data[1].glucose;
+            change = now.glucose - old.glucose;
+        } else if (typeof last !== 'undefined' && last.glucose > 30) {
+            minutes = 5;
+            change = now.glucose - last.glucose;
         } else { change = 0; }
         // multiply by 5 to get the same units as delta, i.e. mg/dL/5m
         avg = change/minutes * 5;
-        var o = {
+
+        return {
             delta: now.glucose - last.glucose
             , glucose: now.glucose
             , avgdelta: avg
         };
-        
-        return o;
-        
     };
 
 
