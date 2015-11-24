8f8d67e9a78a29d1cf9d07bcc94ee0a714db0b72
Scott Leibrand <scottleibrand@gmail.com>
2015-11-24 14:23:55 -0800

    should temp to 0 when LOW w/ positive IOB

--- tests/determine-basal.test.js
+++ tests/determine-basal.test.js
@@ -151,6 +151,15 @@ describe('determine-basal', function ( ) {
         output.reason.should.match(/no temp, setting/);
     });
 
+    it('should temp to 0 when LOW w/ positive IOB', function () {
+        var glucose_status = {"delta":0,"glucose":39,"avgdelta":0};
+        var iob_data = {"iob":1,"activity":0.01,"bolusiob":0.5};
+        var output = determinebasal.determine_basal(glucose_status, currenttemp, iob_data, profile);
+        output.rate.should.equal(0);
+        output.duration.should.equal(30);
+        output.reason.should.match(/BG 39<80/);
+    });
+
     // low eventualBG
 
     it('should low-temp when eventualBG < min_bg', function () {
