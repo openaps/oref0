4120d8e197efec78d71ffc8bf1c823f06e411422
Ben West <bewest@gmail.com>
2016-05-29 17:15:49 -0700

    tweak help output a bit more

--- bin/oref0.sh
+++ bin/oref0.sh
@@ -22,6 +22,7 @@ Valid commands:
   oref0 alias-helper  - <name> <spec>  : create/template a alias from bash commands easily
   oref0 cron-5-minute-helper  - <cmds> - generate a cron template for commands
                                          to run every 5 minutes:
+                                         oref0 cron-5-minute-helper openaps do-everything
   oref0 env                            - print information about environment.
   oref0 pebble
   oref0 ifttt-notify
@@ -29,7 +30,10 @@ Valid commands:
   oref0 calculate-iob
   oref0 meal
   oref0 determine-basal
-  oref0 export-loop  - Print a backup json representation of entire configuration.
+  oref0 export-loop [backup-loop.json] - Print a backup json representation of
+                                         entire configuration. Optionally, if a
+                                         filename is specified, listing is
+                                         saved in the file instead.
   oref0 help - this message
 EOF
 }
