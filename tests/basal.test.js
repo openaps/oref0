c567313acfa564a62af564f79e26a8f85adf0f4a
Sulka Haro <sulka@sulka.net>
2016-08-27 21:36:28 +0300

    Less spammy logging and a test for finding current basal

--- tests/basal.test.js
+++ tests/basal.test.js
@@ -6,26 +6,30 @@ var moment = require('moment');
 
 describe('Basal', function ( ) {
 
- it('should find the right max basal', function() {
-
     var basalprofile = [{'i': 0, 'start': '00:00:00', 'rate': 0, 'minutes': 0},
         {'i': 1, 'start': '00:15:00', 'rate': 2, 'minutes': 15 },
         {'i': 1, 'start': '00:45:00', 'rate': 0.5, 'minutes': 45 }];
 
-	var inputs = {'basals': basalprofile}
+ it('should find the right max daily basal', function() {
 
+	var inputs = {'basals': basalprofile}
 	var basal = require('../lib/profile/basal');
-	
-	console.log(basal);
-	
 	var maxBasal = basal.maxDailyBasal(inputs);
-	
-	console.log(maxBasal);
-	
+		
 	maxBasal.should.equal(2);
 	
   });
 
 
+ it('should find the right basal for a given moment', function() {
+
+	var inputs = {'basals': basalprofile}
+	var startingPoint = new Date(moment("2016-06-13 00:20:00.000").format());
+	var basal = require('../lib/profile/basal');
+	var basal = basal.basalLookup(basalprofile,startingPoint);
+	
+	basal.should.equal(2);
+	
+  });
 
 });
