5f53a23559c11d8b13971a756bf90fbf2eea071c
Sulka Haro <sulka@sulka.net>
2016-09-08 19:04:02 +0300

    Added a conditional-run script to simplify the one liners

---
+++ bin/oref0-conditional-run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export GENERATE=false
+export FILE=$1
+export FILE2=$2
+
+if test ! -n "$FILE"; then
+  echo "Usage: oref0-crun <target report> <age-check report>"
+  exit
+fi
+
+if test ! -f $FILE; then
+  echo "$FILE not found, invoking report"
+  GENERATE=true
+fi
+
+if test -n "$(find settings/ -size -5c | grep $FILE)"; then
+  echo "$FILE under 5 bytes, invoking report"
+  GENERATE=true
+fi
+
+if test -n "$FILE2"; then
+  if test -n "$(find settings/ -newer $FILE | grep $FILE2)"; then
+    echo "$FILE older than $FILE2, invoking report"
+    GENERATE=true
+  fi
+fi
+
+if [ "$GENERATE" = true ]; then
+  openaps invoke $FILE
+else
+  echo "$FILE exists and is new, skipping invoke"
+fi
