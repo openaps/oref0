b4b9a301c5fa36d76b370fdbfa349d81578c02ad
Scott <scottleibrand@gmail.com>
2016-10-03 00:23:43 -0700

    round times to 2 decimal places

--- bin/oref0-dex-time-since.sh
+++ bin/oref0-dex-time-since.sh
@@ -2,5 +2,5 @@
 
 GLUCOSE=$1
 
-cat $GLUCOSE | json -e "this.minAgo=(new Date()-new Date(this.dateString))/60/1000" | json -a minAgo | head -n 1
+cat $GLUCOSE | json -e "this.minAgo=Math.round(100*(new Date()-new Date(this.dateString))/60/1000)/100" | json -a minAgo | head -n 1
 
