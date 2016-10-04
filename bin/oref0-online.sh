1587cbc797c5afba43f0c4e83eeacb7be025342d
Scott <scottleibrand@gmail.com>
2016-10-03 21:47:00 -0700

    check if rig is online; cycle wlan0 or set up BT tethering if not

---
+++ bin/oref0-online.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+MAC=$1
+echo -n "At $(date) my IP is: "
+if ! curl -m 15 icanhazip.com; then
+    echo -n "Error, cycling wlan0 "
+    sudo ifdown wlan0
+    sudo ifup wlan0
+    echo -n "and getting new wlan0 IP"
+    ps aux | grep -v grep | grep -q "dhclient wlan0" && sudo killall dhclient
+    sudo dhclient wlan0 -r
+    sudo dhclient wlan0
+    echo
+    echo -n "At $(date) my IP is: "
+    if ! curl -m 15 icanhazip.com; then
+        echo -n "Error, connecting BT to $MAC "
+        sudo bt-pan client $MAC
+        echo -n "and getting bnep0 IP"
+        sudo dhclient bnep0
+        echo
+        echo -n "At $(date) my IP is: "
+        curl -m 15 icanhazip.com
+        echo
+    fi
+fi
