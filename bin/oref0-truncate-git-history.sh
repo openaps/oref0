ed810f3511fc9bc71ba508c8197671f29d6edb65
Scott <scottleibrand@gmail.com>
2016-11-05 21:11:29 -0700

    proceed if nothing to commit

--- bin/oref0-truncate-git-history.sh
+++ bin/oref0-truncate-git-history.sh
@@ -37,7 +37,8 @@ find .git/index.lock -mmin +5 -exec rm {} \; 2>/dev/null
 commits=$(git log | grep -c commit)
 if (( $commits > 1500 )); then
     echo "Found $commits commits; truncating git history"
-    git commit -m"save unsaved changes" -a && du -sh .git && git rev-parse HEAD~1000 > .git/info/grafts && git filter-branch -- --all
+    git commit -m"save unsaved changes" -a 
+    du -sh .git && git rev-parse HEAD~1000 > .git/info/grafts && git filter-branch -- --all
     rm .git/info/grafts
     git update-ref -d refs/original/refs/heads/master && git reflog expire --expire=now --all && git gc --prune=now --aggressive 
 fi 
