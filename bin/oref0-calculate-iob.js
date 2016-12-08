474208b619f2b8bc430e836d0eb897f0ab5d05b9
Paul Dickens <dickens.paul@gmail.com>
2016-12-08 20:39:22 +1100

    allow oref0-calculate-iob to optionally use mmhistorytools prepared history

--- bin/oref0-calculate-iob.js
+++ bin/oref0-calculate-iob.js
@@ -18,20 +18,31 @@
 
 */
 
-var generate = require('oref0/lib/iob');
-function usage ( ) {
-    console.log('usage: ', process.argv.slice(0, 2), '<pumphistory-zoned.json> <profile.json> <clock-zoned.json>');
-
-}
-
 if (!module.parent) {
-  var pumphistory_input = process.argv.slice(2, 3).pop();
+  var argv = require('yargs')
+    .usage("$0 pumphistory-zoned.json profile.json clock-zoned.json [--prepared]")
+    .option('prepared', {
+      alias: 'p',
+      describe: "Pump history prepared using mmhistorytools",
+      boolean: true
+    })
+    // error and show help if some other args given
+    .strict(true)
+    .help('help')
+    .argv
+  ;
+
+  function usage ( ) {
+    argv.showHelp( );
+  }
+
+  var pumphistory_input = argv._.slice(0, 1).pop();
   if ([null, '--help', '-h', 'help'].indexOf(pumphistory_input) > 0) {
     usage( );
     process.exit(0)
   }
-  var profile_input = process.argv.slice(3, 4).pop();
-  var clock_input = process.argv.slice(4, 5).pop();
+  var profile_input = argv._.slice(1, 2).pop();
+  var clock_input = argv._.slice(2, 3).pop();
 
   if (!pumphistory_input || !profile_input) {
     usage( );
@@ -42,6 +53,7 @@ if (!module.parent) {
   var all_data = require(cwd + '/' + pumphistory_input);
   var profile_data = require(cwd + '/' + profile_input);
   var clock_data = require(cwd + '/' + clock_input);
+  var generate = (argv.prepared) ? require('oref0/lib/iob-prepared') : require('oref0/lib/iob');
 
   // all_data.sort(function (a, b) { return a.date > b.date });
 
@@ -54,4 +66,3 @@ if (!module.parent) {
   var iob = generate(inputs);
   console.log(JSON.stringify(iob));
 }
-
