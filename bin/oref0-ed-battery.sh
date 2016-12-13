af7d3cd61721671766f8a6b73b2fa5e36651f1f9
John Benjamin <johnbenjamin1061@gmail.com>
2016-12-13 18:46:51 -0500

    Update oref0-ed-battery.sh

--- bin/oref0-ed-battery.sh
+++ bin/oref0-ed-battery.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo Run oref0-ed-battery
 if /home/edison/src/EdisonVoltage/voltage json batteryVoltage battery | grep "-66" 2>/dev/null; then
     echo start Voltage server and trigger
     cd /home/edison/src/EdisonVoltage && sudo ./voltage_server &
