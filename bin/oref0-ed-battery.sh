6ebc22345fbab06de887080caf534327dfabb6f6
John Benjamin <johnbenjamin1061@gmail.com>
2016-12-14 13:52:06 -0500

    Delete oref0-ed-battery.sh
    not needed

--- bin/oref0-ed-battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo Run oref0-ed-battery
-if ! /home/edison/src/EdisonVoltage/voltage json batteryVoltage battery | grep "-66" 2>/dev/null; then
-    echo start Voltage
-    sudo /home/edison/src/EdisonVoltage/voltage
-fi
