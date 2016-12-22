b23b94bd969fb6bb717cdbd03ca20a5f9ca16ec8
kenstack <kenstack@yahoo.com>
2016-12-21 20:13:29 -0500

    restart networking completely instead just cycling wlan0
    this has proved more stable for me across some wifi netoworks

--- bin/oref0-online.sh
+++ bin/oref0-online.sh
@@ -2,13 +2,17 @@
 MAC=$1
 echo -n "At $(date) my IP is: "
 if ! curl -m 15 icanhazip.com; then
-    echo -n "Error, cycling wlan0 "
-    sudo ifdown wlan0
-    sudo ifup wlan0
-    echo -n "and getting new wlan0 IP"
-    ps aux | grep -v grep | grep -q "dhclient wlan0" && sudo killall dhclient
-    sudo dhclient wlan0 -r
-    sudo dhclient wlan0
+    echo -n "Error, cycling networking "
+   # sudo ifdown wlan0
+  #  sudo ifup wlan0
+  #  echo -n "and getting new wlan0 IP"
+   # ps aux | grep -v grep | grep -q "dhclient wlan0" && sudo killall dhclient
+   # sudo dhclient wlan0 -r
+  #  sudo dhclient wlan0
+  #modify prior script to simply restart networking completely for stability purposes
+  /etc/init.d/networking stop
+  sleep 5
+  /etc/init.d/networking start
     echo
     echo -n "At $(date) my IP is: "
     if ! curl -m 15 icanhazip.com; then
