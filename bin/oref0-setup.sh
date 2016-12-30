4b8f34d6fe6ce9cadd6688e7093d2d683ba8f2c2
PieterGit <pieterg@gmail.com>
2016-12-30 11:11:35 +0100

    move logrotate install to postinstall, deprecate global-install script

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -241,7 +241,7 @@ mkdir -p settings || die "Can't mkdir settings"
 mkdir -p enact || die "Can't mkdir enact"
 mkdir -p upload || die "Can't mkdir upload"
 if [[ ${CGM,,} =~ "xdrip" ]]; then
-	mkdir -p xdrip || die "Can't mkdir xdrip"
+    mkdir -p xdrip || die "Can't mkdir xdrip"
 fi
 
 mkdir -p $HOME/src/
@@ -251,17 +251,18 @@ if [ -d "$HOME/src/oref0/" ]; then
     OLD_HEAD=$(git rev-parse HEAD)
     (git fetch && git pull) || die "Couldn't pull latest oref0"
     NEW_HEAD=$(git rev-parse HEAD)
-    if [ "$OLD_HEAD" == "$NEW_HEAD" ];  then
+    if [ "$OLD_HEAD" != "$NEW_HEAD" ];  then
        echo "(Re)installing oref0 installation from ~/src/oref0. version=$NEW_HEAD"
-       npm run global-install
+       sudo npm install -g .
+       #npm run global-install
     else 
        echo "Not need to reinstall. version=$NEW_HEAD is still current"
     fi
 else
    echo -n "Cloning oref0: "
-    (cd ~/src && git clone git://github.com/openaps/oref0.git) || die "Couldn't clone oref0"
-    echo Checking oref0 installation
-    npm list -g oref0 | egrep oref0@0.3. || (echo Installing latest oref0 && sudo npm install -g oref0)
+   (cd ~/src && git clone git://github.com/openaps/oref0.git) || die "Couldn't clone oref0"
+   echo Checking oref0 installation
+   npm list -g oref0 | egrep oref0@0.3. || (echo Installing latest oref0 && sudo npm install -g oref0)
 fi
 
 echo Checking mmeowlink installation
@@ -280,9 +281,10 @@ fi
 cat preferences.json
 git add preferences.json
 
-# enable log rotation
-sudo cp $HOME/src/oref0/logrotate.openaps /etc/logrotate.d/openaps || die "Could not cp /etc/logrotate.d/openaps"
-sudo cp $HOME/src/oref0/logrotate.rsyslog /etc/logrotate.d/rsyslog || die "Could not cp /etc/logrotate.d/rsyslog"
+#enable log rotation
+#DEPRECATED: See https://github.com/openaps/oref0/pull/302
+#sudo cp $HOME/src/oref0/logrotate.openaps /etc/logrotate.d/openaps || die "Could not cp /etc/logrotate.d/openaps"
+#sudo cp $HOME/src/oref0/logrotate.rsyslog /etc/logrotate.d/rsyslog || die "Could not cp /etc/logrotate.d/rsyslog"
 
 test -d /var/log/openaps || sudo mkdir /var/log/openaps && sudo chown $USER /var/log/openaps || die "Could not create /var/log/openaps"
 
