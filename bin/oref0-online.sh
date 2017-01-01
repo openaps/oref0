aed5d31e629fa6306957071248094153b1b7d848
kenstack <kenstack@yahoo.com>
2017-01-01 16:08:42 -0500

    restart networking completely instead just cycling wlan0 (#284)
    * restart networking completely instead just cycling wlan0
    
    this has proved more stable for me across some wifi netoworks
    
    * Re-add dhclient release/renew
    
    * Update oref0-online.sh

--- bin/oref0-online.sh
+++ bin/oref0-online.sh
@@ -2,9 +2,11 @@
 MAC=$1
 echo -n "At $(date) my IP is: "
 if ! curl -m 15 icanhazip.com; then
-    echo -n "Error, cycling wlan0 "
-    sudo ifdown wlan0
-    sudo ifup wlan0
+    echo -n "Error, cycling networking "
+    # simply restart networking completely for stability purposes
+    /etc/init.d/networking stop
+    sleep 5
+    /etc/init.d/networking start
     echo -n "and getting new wlan0 IP"
     ps aux | grep -v grep | grep -q "dhclient wlan0" && sudo killall dhclient
     sudo dhclient wlan0 -r
@@ -13,7 +15,7 @@ if ! curl -m 15 icanhazip.com; then
     echo -n "At $(date) my IP is: "
     if ! curl -m 15 icanhazip.com; then
         echo -n "Error, connecting BT to $MAC "
-        sudo killall bluetoothd; sudo /usr/local/bin/bluetoothd --experimental &
+        sudo killall bluetoothd; sleep 5; sudo /usr/local/bin/bluetoothd --experimental &
         sudo bt-pan client $MAC
         echo -n "and getting bnep0 IP"
         sudo dhclient bnep0
