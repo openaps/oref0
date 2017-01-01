8d6a0dc7a877798fa2cbf56bb2d1a4bfd7e06fdb
Scott Leibrand <scottleibrand@gmail.com>
2016-12-31 18:03:18 -0600

    Re-add dhclient release/renew

--- bin/oref0-online.sh
+++ bin/oref0-online.sh
@@ -3,21 +3,20 @@ MAC=$1
 echo -n "At $(date) my IP is: "
 if ! curl -m 15 icanhazip.com; then
     echo -n "Error, cycling networking "
-   # sudo ifdown wlan0
-  #  sudo ifup wlan0
-  #  echo -n "and getting new wlan0 IP"
-   # ps aux | grep -v grep | grep -q "dhclient wlan0" && sudo killall dhclient
-   # sudo dhclient wlan0 -r
-  #  sudo dhclient wlan0
-  #modify prior script to simply restart networking completely for stability purposes
-  /etc/init.d/networking stop
-  sleep 5
-  /etc/init.d/networking start
+    # simply restart networking completely for stability purposes
+    /etc/init.d/networking stop
+    sleep 5
+    /etc/init.d/networking start
+    echo -n "and getting new wlan0 IP"
+    ps aux | grep -v grep | grep -q "dhclient wlan0" && sudo killall dhclient
+    sudo dhclient wlan0 -r
+    sudo dhclient wlan0
+
     echo
     echo -n "At $(date) my IP is: "
     if ! curl -m 15 icanhazip.com; then
         echo -n "Error, connecting BT to $MAC "
-        sudo killall bluetoothd; sudo /usr/local/bin/bluetoothd --experimental &
+        sudo killall bluetoothd; sleep 5; sudo /usr/local/bin/bluetoothd --experimental &
         sudo bt-pan client $MAC
         echo -n "and getting bnep0 IP"
         sudo dhclient bnep0
