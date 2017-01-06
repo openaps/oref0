563e29b5b92a5a356e21a39b557082cdb0efd142
John Benjamin <johnbenjamin1061@gmail.com>
2017-01-05 20:33:08 -0500

    Delete pan-urchin-status.sh

--- bin/pan-urchin-status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MAC=$1
-
-sudo rfcomm bind hci0 $MAC
-
-#Status for Pancreabble Urchin
-#echo {"\"message\": "\"loop status at "'$(date +%-I:%M%P)'": Running\"} > upload/urchin-status.json
-echo {"\"message\": "\""$(date +%R)": IOB: $(jq .openaps.iob.iob upload/ns-status.json) - BasalIOB: $(jq .openaps.iob.basaliob upload/ns-status.json)\"} > upload/urchin-status.json
