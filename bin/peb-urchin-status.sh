26c5b149ee0c913c1699b81f1dd794b85a0170f8
John Benjamin <johnbenjamin1061@gmail.com>
2017-01-07 21:01:40 -0500

    Update peb-urchin-status.sh

--- bin/peb-urchin-status.sh
+++ bin/peb-urchin-status.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 MAC=$1
 
-sudo rfcomm bind hci0 $MAC
+if ! ( rfcomm show hci0 | grep -q $MAC ) ; then
+   sudo rfcomm bind hci0 $MAC
+fi 
 
 #Status for Pancreabble Urchin
 #echo {"\"message\": "\"loop status at "'$(date +%-I:%M%P)'": Running\"} > upload/urchin-status.json
 echo {"\"message\": "\""$(date +%R)": IOB: $(jq .openaps.iob.iob upload/ns-status.json) - BasalIOB: $(jq .openaps.iob.basaliob upload/ns-status.json)\"} > upload/urchin-status.json
-
