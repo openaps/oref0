a92c5364f37892d20bee9adbe84912439a00c614
John Benjamin <johnbenjamin1061@gmail.com>
2017-01-10 12:46:16 -0500

    Update oref0-bluetoothup.sh
    change to allow the bluetooth pan to get shutdown if the rig sees that there is an ip address on the wlan0. Script will also get the rig ready to run the bt-pan if wifi not available.

--- bin/oref0-bluetoothup.sh
+++ bin/oref0-bluetoothup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+if ( ifconfig wlan0 | grep -q "inet addr" ) && ( ifconfig bnep0 | grep -q "inet addr" ); then
+   sudo killall bluetoothd
+fi
 
 if ! ( hciconfig -a | grep -q "PSCAN" ) ; then
    sudo killall bluetoothd
@@ -8,6 +11,7 @@ fi
 
 if ( hciconfig -a | grep -q "DOWN" ) ; then
    sudo hciconfig hci0 up
+   sudo /usr/local/bin/bluetoothd --experimental &
 fi
 
 if !( hciconfig -a | grep -q $HOSTNAME ) ; then
