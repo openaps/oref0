c1980a6279b0fc107c70afc4d4f180f2b41f1bd3
Scott Leibrand <scottleibrand@gmail.com>
2017-01-17 19:20:00 -0800

    mmtune; multi-line for readability; remove ;'s

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -7,6 +7,15 @@ if [ -z $port ]; then
     port=/dev/spidev5.1
 fi
 
+function mmtune {
+    reset_spi_serial.py 2>/dev/null
+    echo {} > monitor/mmtune.json
+    echo -n \"mmtune: \" && openaps report invoke monitor/mmtune.json 2>/dev/null >/dev/null
+    grep -v setFreq monitor/mmtune.json | grep -A2 $(json -a setFreq -f monitor/mmtune.json) | while read line
+        do echo -n \"$line \"
+    done
+}
+
 function wait_for_silence {
     if [ -z $1 ]; then
         waitfor=30
@@ -14,7 +23,7 @@ function wait_for_silence {
         waitfor=$1
     fi
     (mmeowlink-any-pump-comms.py --port $port --wait-for 1 | grep -q comms && echo -n Radio ok, || openaps mmtune) \
-    && echo -n \" Listening: \"; 
+    && echo -n \" Listening: \"
     for i in $(seq 1 100); do 
         echo -n .
         mmeowlink-any-pump-comms.py --port $port --wait-for $waitfor 2>/dev/null | egrep -v subg | egrep No \
@@ -37,9 +46,9 @@ function enact {
     && if (cat enact/suggested.json && grep -q duration enact/suggested.json); then ( 
         rm enact/enacted.json
         openaps report invoke enact/enacted.json
-        grep -q duration enact/enacted.json || openaps invoke enact/enacted.json ) 2>&1 | egrep -v \"^  |subg_rfspy|handler\"; 
-    fi;
-    grep incorrectly enact/suggested.json && oref0-set-system-clock 2>/dev/null;
+        grep -q duration enact/enacted.json || openaps invoke enact/enacted.json ) 2>&1 | egrep -v \"^  |subg_rfspy|handler\"
+    fi
+    grep incorrectly enact/suggested.json && oref0-set-system-clock 2>/dev/null
     cat enact/enacted.json | json -0
 }
 
@@ -59,14 +68,26 @@ function refresh_old_profile {
     || (echo -n Old settings refresh && openaps get-settings 2>/dev/null >/dev/null && echo ed )
 }
 
-# TODO: make multi-line for readability
 function refresh_temp_and_enact {
-    if( (find monitor/ -newer monitor/temp_basal.json | grep -q glucose.json && echo glucose.json newer than temp_basal.json ) || (! find monitor/ -mmin -5 -size +5c | grep -q temp_basal && echo temp_basal.json more than 5m old)); then (echo -n Temp refresh && openaps report invoke monitor/temp_basal.json monitor/clock.json monitor/clock-zoned.json monitor/iob.json 2>/dev/null >/dev/null && echo ed && if(cat monitor/temp_basal.json | json -c \"this.duration < 27\" | grep -q duration); then openaps enact; else echo Temp duration 27m or more; fi); else echo temp_basal.json less than 5m old; fi
+    if( (find monitor/ -newer monitor/temp_basal.json | grep -q glucose.json && echo glucose.json newer than temp_basal.json ) \
+        || (! find monitor/ -mmin -5 -size +5c | grep -q temp_basal && echo temp_basal.json more than 5m old)); then
+            (echo -n Temp refresh && openaps report invoke monitor/temp_basal.json monitor/clock.json monitor/clock-zoned.json monitor/iob.json 2>/dev/null >/dev/null && echo ed \
+            && if (cat monitor/temp_basal.json | json -c \"this.duration < 27\" | grep -q duration); then
+                openaps enact; else echo Temp duration 27m or more
+            fi)
+    else
+        echo temp_basal.json less than 5m old
+    fi
 }
 
-# TODO: make multi-line for readability
 function refresh_pumphistory_and_enact {
-    if ((find monitor/ -newer monitor/pumphistory-zoned.json | grep -q glucose.json && echo -n glucose.json newer than pumphistory) || (find enact/ -newer monitor/pumphistory-zoned.json | grep -q enacted.json && echo -n enacted.json newer than pumphistory) || (! find monitor/ -mmin -5 | grep -q pumphistory-zoned && echo -n pumphistory more than 5m old) ); then (echo -n \": \" && openaps gather && openaps enact ); else echo Pumphistory less than 5m old; fi
+    if ((find monitor/ -newer monitor/pumphistory-zoned.json | grep -q glucose.json && echo -n glucose.json newer than pumphistory) \
+        || (find enact/ -newer monitor/pumphistory-zoned.json | grep -q enacted.json && echo -n enacted.json newer than pumphistory) \
+        || (! find monitor/ -mmin -5 | grep -q pumphistory-zoned && echo -n pumphistory more than 5m old) ); then
+            (echo -n \": \" && openaps gather && openaps enact )
+    else
+        echo Pumphistory less than 5m old
+    fi
 }
 
 function refresh_profile {
@@ -97,6 +118,6 @@ until( \
 echo Error, retrying \
 && [[ $RANDOM > 30000 ]] \
 && wait_for_silence 45 \
-&& openaps mmtune; 
-sleep 5;
+&& mmtune
+sleep 5
 done
