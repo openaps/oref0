ea8f411d32f3b1abb79d1ebe9a1181a80561aac4
Scott <scottleibrand@gmail.com>
2017-01-29 17:25:12 -0800

    do this in oref0-online instead

--- bin/oref0-bluetoothup.sh
+++ bin/oref0-bluetoothup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 MAC=$1
 
-if ( ifconfig wlan0 | grep -q "inet addr" ) && ( ifconfig bnep0 | grep -q "inet addr" ); then
-   sudo bt-pan client $MAC -d
-fi
-
 if ! ( hciconfig -a | grep -q "PSCAN" ) ; then
    sudo killall bluetoothd
    sudo /usr/local/bin/bluetoothd --experimental &
