9870ca2d9bf75ae0a75e952a8b8739dac92f3280
Scott <scottleibrand@gmail.com>
2017-01-30 22:01:05 -0800

    mmtune 25% of the time

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -204,8 +204,8 @@ function mmtune {
 }
 
 function maybe_mmtune {
-    # mmtune 8% of the time ((32k-30000)/32k)
-    [[ $RANDOM > 30000 ]] \
+    # mmtune 25% of the time ((32k-24576)/32k)
+    [[ $RANDOM > 24576 ]] \
     && echo "Waiting for 30s silence before mmtuning" \
     && wait_for_silence 30 \
     && mmtune
