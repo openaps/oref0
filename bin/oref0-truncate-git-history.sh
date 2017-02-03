43b19b596896d9501586db6d8bb9959d2831f73d
Scott <scottleibrand@gmail.com>
2017-02-02 23:39:31 -0800

    kill off any existing git operations so we can gc

--- bin/oref0-truncate-git-history.sh
+++ bin/oref0-truncate-git-history.sh
@@ -42,7 +42,7 @@ if (( $commits > 5000 )); then
     #du -sh .git && \
     git rev-parse HEAD~2000 > .git/info/grafts && git filter-branch -- --all
     rm .git/info/grafts
-    git update-ref -d refs/original/refs/heads/master && git reflog expire --expire=now --all && git gc --prune=now --aggressive 
+    git update-ref -d refs/original/refs/heads/master && git reflog expire --expire=now --all && (killall -g git; git gc --prune=now --aggressive)
 fi 
 commits=$(git log | grep -c commit)
 usage=$(du -sh .git | awk '{print $1}')
