6ea24bb9fd2df2cc1f0e4c8bf62c295df724773f
Scott <scottleibrand@gmail.com>
2017-02-02 23:29:22 -0800

    only run du after truncating

--- bin/oref0-truncate-git-history.sh
+++ bin/oref0-truncate-git-history.sh
@@ -39,7 +39,8 @@ commits=$(git log | grep -c commit)
 if (( $commits > 5000 )); then
     echo "Found $commits commits; truncating git history"
     git commit -m"save unsaved changes" -a 
-    du -sh .git && git rev-parse HEAD~2000 > .git/info/grafts && git filter-branch -- --all
+    #du -sh .git && \
+    git rev-parse HEAD~2000 > .git/info/grafts && git filter-branch -- --all
     rm .git/info/grafts
     git update-ref -d refs/original/refs/heads/master && git reflog expire --expire=now --all && git gc --prune=now --aggressive 
 fi 
