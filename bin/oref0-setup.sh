65cdd1527311825cef1cdfabdfcd281c5d54ba21
Scott Leibrand <scottleibrand@gmail.com>
2017-02-04 21:56:06 -0800

    save crontab to ~/crontab.txt and prompt to crontab -r before recreating (#377)
    * save crontab to ~/crontab.txt and prompt to crontab -r before recreating
    
    * save both crontab.old.txt and crontab.txt

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -636,6 +636,14 @@ echo
 
 read -p "Schedule openaps in cron? y/[N] " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+    echo Saving existing crontab to $HOME/crontab.txt:
+    crontab -l | tee $HOME/crontab.old.txt
+    read -p "Would you like to remove your existing crontab first? y/[N] " -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        crontab -r
+    fi
+
 # add crontab entries
     (crontab -l; crontab -l | grep -q "$NIGHTSCOUT_HOST" || echo NIGHTSCOUT_HOST=$NIGHTSCOUT_HOST) | crontab -
     (crontab -l; crontab -l | grep -q "API_SECRET=" || echo API_SECRET=$(nightscout hash-api-secret $API_SECRET)) | crontab -
@@ -679,7 +687,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     if [[ ! -z "$BT_PEB" || ! -z "$BT_MAC" ]]; then
        (crontab -l; crontab -l | grep -q "oref0-bluetoothup" || echo '* * * * * ps aux | grep -v grep | grep -q "oref0-bluetoothup" || oref0-bluetoothup >> /var/log/openaps/network.log' ) | crontab -
     fi
-    crontab -l
+    crontab -l | tee $HOME/crontab.txt
 fi
 
 if [[ ${CGM,,} =~ "shareble" ]]; then
