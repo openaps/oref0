003dc5154b7b7bbd309de91d7b32e0aa66b497a5
Dana Lewis <danamichellelewis@gmail.com>
2017-02-05 17:07:05 -0800

    Attempting to add explorer board prompt to setup script to resolve common error and confusion

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -145,15 +145,23 @@ if [[ -z "$DIR" || -z "$serial" ]]; then
         BLE_SERIAL=$REPLY
         echo "$BLE_SERIAL? Got it."
     fi
-    read -p "Are you using mmeowlink? If not, press enter. If so, what TTY port (full port address, looks like "/dev/ttySOMETHING" without the quotes - you probably want to copy paste it)? " -r
-    ttyport=$REPLY
-    echo -n "Ok, "
-    if [[ -z "$ttyport" ]]; then
-        echo -n Carelink
+
+if
+    read -p "Are you using an Explorer Board? [Y/n] -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ttyport=$/dev/spidev5.1
     else
-        echo -n TTY $ttyport
-    fi
-    echo " it is."
+        read -p "Are you using mmeowlink (i.e. with a TI stick)? If not, press enter. If so, what TTY port (full port address, looks like "/dev/ttySOMETHING" without the quotes - you probably want to copy paste it)? " -r
+        ttyport=$REPLY
+        echo -n "Ok, "
+        if [[ -z "$ttyport" ]]; then
+            echo -n Carelink
+        else
+            echo -n TTY $ttyport
+        fi
+        echo " it is."
+fi
+
 
     if [[ ! -z "${ttyport}" ]]; then
       echo "Medtronic pumps come in two types: WW (Worldwide) pumps, and NA (North America) pumps."
