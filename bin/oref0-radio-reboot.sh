e4c80eb227d3514c3efb8ddeaa355b4ab46474be
Scott <scottleibrand@gmail.com>
2017-02-22 00:14:30 -0800

    print extra newlines to clear old spidev5.1 errors faster

--- bin/oref0-radio-reboot.sh
+++ bin/oref0-radio-reboot.sh
@@ -1,9 +1,11 @@
 radio_errors=`tail /var/log/openaps/pump-loop.log | grep "spidev5.1 already in use"`
 if [ ! -z "$radio_errors" ]; then
     logfile=/var/log/openaps/pump-loop.log
+    echo >> $logfile
+    echo -n "Radio error found at " | tee -a $logfile
     date >> $logfile
-    echo "Radio error found" | tee -a $logfile
     shutdown -r +5 "Rebooting to fix radio errors!" | tee -a $logfile
+    echo >> $logfile
 else
     if [ -e /run/nologin ]; then
         echo "No more radio errors; canceling reboot" | tee -a $logfile
