f186f88a2bede407fe7b70e954400ccc7fdb2163
Scott <scottleibrand@gmail.com>
2017-04-28 11:57:50 -0700

    allow temp rate within 0.025U/hr for decocare floating point math

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -140,13 +140,13 @@ function smb_enact_temp {
 
 function smb_verify_enacted {
     # Read the currently running temp and
-    # verify rate matches and duration is no shorter than 5m less than smb-suggested.json
+    # verify rate matches (within 0.03U/hr) and duration is no shorter than 5m less than smb-suggested.json
     rm -rf monitor/temp_basal.json
     ( echo -n Temp refresh \
         && ( openaps report invoke monitor/temp_basal.json || openaps report invoke monitor/temp_basal.json ) \
         2>&1 >/dev/null | tail -1 && echo -n "ed: " \
     ) && echo -n "monitor/temp_basal.json: " && cat monitor/temp_basal.json | jq -C -c . \
-    && jq --slurp --exit-status 'if .[1].rate then (.[0].rate == .[1].rate and .[0].duration > .[1].duration - 5) else true end' monitor/temp_basal.json enact/smb-suggested.json > /dev/null
+    && jq --slurp --exit-status 'if .[1].rate then (.[0].rate > .[1].rate - 0.03 and .[0].rate < .[1].rate + 0.03 and .[0].duration > .[1].duration - 5) else true end' monitor/temp_basal.json enact/smb-suggested.json > /dev/null
 }
 
 function smb_verify_reservoir {
