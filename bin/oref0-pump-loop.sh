05624403e2db8c6aacfae14eb4dbdd363a98a00d
Scott <scottleibrand@gmail.com>
2017-04-29 13:16:43 -0700

    wait for a full 30s silence before mmtuning

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -248,8 +248,8 @@ function mmtune {
 function maybe_mmtune {
     # mmtune 25% of the time ((32k-24576)/32k)
     [[ $RANDOM > 24576 ]] \
-    && echo "Waiting for $upto30s second silence before mmtuning" \
-    && wait_for_silence $upto30s \
+    && echo "Waiting for 30s silence before mmtuning" \
+    && wait_for_silence 30 \
     && mmtune
 }
 
