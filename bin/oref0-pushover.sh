9f6bde20f45e7f2a257041185c6e13322471f87e
Scott <scottleibrand@gmail.com>
2017-05-04 10:22:33 -0700

    usage

--- bin/oref0-pushover.sh
+++ bin/oref0-pushover.sh
@@ -6,6 +6,10 @@ FILE={$3-enact/smb-suggested.json}
 SOUND={$4-none}
 SNOOZE={$5-15}
 
+if [ -z $TOKEN ] || [ -z $USER ]; then
+      echo "Usage: $0 <TOKEN> <USER> [enact/smb-suggested.json] [none] [15]"
+fi
+
 if ! find monitor/ -mmin -$SNOOZE -ls | grep pushover-sent && cat $FILE | egrep "add'l|maxBolus"
     curl -s -F "token=$TOKEN" -F "user=$USER" -F "sound=$SOUND" -F "message=$(jq -c "{bg, tick, carbsReq, insulinReq, reason}|del(.[] | nulls)" $FILE)" https://api.pushover.net/1/messages.json && touch monitor/pushover-sent
 fi
