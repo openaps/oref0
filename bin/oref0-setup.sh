openaps/oref0

commit c2cfc8fb5e11164e9c5cdfb28febeebe56147a4c

Add Shortcuts for Log files to help with terminal (#402)
* Update oref0-setup.sh

added all log files shortcuts to .bash_profile

* Update oref0-setup.sh

fix errors

* Update oref0-setup.sh

to avoid having wrong command run added log to end of alias

* Sort treatments before calculating COB (#404)

* sort treatments

* sort treatments by actual datestamp

* Create oref0-log-shortcuts.sh

bash Script to add shortcuts to the OpenAPS logs

* Update oref0-setup.sh

Change log shortcuts to run during install from a Bash script.

* Update package.json

add oref0-log-shortcuts

* Update oref0-setup.sh

* Automatically copy pumpprofile.json & autotune.json from profile.json (#392)

* auto copy pumpprofile.json & autotune.json from profile.json

* allow manual users to run autotune by simply creating a settings/pumpprofile.json file, #392

* simple -> simply

* Autotune: Porting to Python - oref0-autotune.py (#403)

* bin/oref0-autotune.py initial

* add notes, make autotune_core call easier to read

* add nightscout log messages

* remove nightscout url validation for now

* ensure uniform date format for filenames

* more date fixes

* missing (

* more missing )s

* add expand-user for DIRs with ~ or ~user

* remove character with different encoding

* fix missing bluetooth command line options in oref0-runagain.sh (#405)

* Install missing jq dependency when --btpeb is set. (#408)

* spidev5.1 already in use reboot (#411)

* oref0-radio-reboot

* oref0-radio-reboot

* oref0-radio-reboot

* wait 5m to reboot; cancel if it recovers by then

* run oref0-radio-reboot in cron two more times before actually rebooting

* /run/nologin only exists for 5m; run every minute

* print extra newlines to clear old spidev5.1 errors faster

* don't extend reboot time every minute

* write cancelation to logfile

* Changes to oref0-online (#396)

Replace how we get the SSID, use iwgetid instead.

bin/oref0-setup.sh
@@ -634,6 +634,9 @@ echo Add NIGHTSCOUT_HOST and API_SECRET to $HOME/.profile
 (cat $HOME/.profile | grep -q "NIGHTSCOUT_HOST" || echo export NIGHTSCOUT_HOST="$NIGHTSCOUT_HOST") >> $HOME/.profile
 (cat $HOME/.profile | grep -q "API_SECRET" || echo export API_SECRET="`nightscout hash-api-secret $API_SECRET`") >> $HOME/.profile
 
+echo "Adding OpenAPS log shortcuts"
+oref0-log-shortcuts
+
 echo
 if [[ "$ttyport" =~ "spi" ]]; then
     echo Resetting spi_serial

bin/oref0-log-shortcuts.sh
@@ -1,0 +1,13 @@
+#!/bin/bash
+
+# add crontab entries
+    grep -q "networklog" ".bash_profile" || echo "alias networklog="'"tail -n 100 -F /var/log/openaps/network.log"' >> ".bash_profile"
+    grep -q "xdrip-looplog" ".bash_profile" || echo "alias xdrip-looplog="'"tail -n 100 -F /var/log/openaps/xdrip-loop.log"' >> ".bash_profile"
+    grep -q "cgm-looplog" ".bash_profile" || echo "alias cgm-looplog="'"tail -n 100 -F /var/log/openaps/cgm-loop.log"' >> ".bash_profile"
+    grep -q "autosens-looplog" ".bash_profile" || echo "alias autosens-looplog="'"tail -n 100 -F /var/log/openaps/autosens-loop.log"' >> ".bash_profile"
+    grep -q "autotunelog" ".bash_profile" || echo "alias autotunelog="'"tail -n 100 -F /var/log/openaps/autotune.log"' >> ".bash_profile"
+    grep -q "pump-looplog" ".bash_profile" || echo "alias pump-looplog="'"tail -n 100 -F /var/log/openaps/pump-loop.log"' >> ".bash_profile"
+    grep -q "urchin-looplog" ".bash_profile" || echo "alias urchin-looplog="'"tail -n 100 -F /var/log/openaps/urchin-loop.log"' >> ".bash_profile"
+
+# to enable shortcut aliases in ~/.bash_profile
+source ~/.bash_profile

commit 639aa56f930a0b0de9408b092ebd838e23fbf25b

fix out of the box working of cli commands with Nightscout interactions
because the ~/.bash_profile takes preferences above ~/.profile, the cli
commands with Nightscout didn't have access to the NIGHTSCOUT_HOST and
API_SECRET environment variables.

This was caused by https://github.com/openaps/oref0/pull/402

use ~/.bash_profile instead of ~/.profile to make

bin/oref0-setup.sh
@@ -702,12 +702,15 @@ if [[ $ENABLE =~ microbolus ]]; then
     done
 fi
 
-# Create ~/.profile so that openaps commands can be executed from the command line
-# as long as we still use enivorement variables it's easy that the openaps commands work from both crontab and from a common shell
-# TODO: remove API_SECRET and NIGHTSCOUT_HOST (see https://github.com/openaps/oref0/issues/299)
-echo Add NIGHTSCOUT_HOST and API_SECRET to $HOME/.profile
-(cat $HOME/.profile | grep -q "NIGHTSCOUT_HOST" || echo export NIGHTSCOUT_HOST="$NIGHTSCOUT_HOST") >> $HOME/.profile
-(cat $HOME/.profile | grep -q "API_SECRET" || echo export API_SECRET="`nightscout hash-api-secret $API_SECRET`") >> $HOME/.profile
+# Append NIGHTSCOUT_HOST and API_SECRET to $HOME/.bash_profile so that openaps commands can be executed from the command line
+# Once all crontab entries are called within a bash environment, then the NIGHTSCOUT_HOST and API_SECRET can be removed from the crontab 
+# TODO: remove API_SECRET and NIGHTSCOUT_HOST (see https://github.com/openaps/oref0/issues/299 )
+# Removing stuff from ~/.profile (old solution) is left as an exercise to the reader
+echo "#Added NIGHTSCOUT_HOST and API_SECRET to $HOME/.bash_profile at `date`" | tee -a $HOME/.bash_profile
+echo NIGHTSCOUT_HOST="$NIGHTSCOUT_HOST" >> $HOME/.bash_profile
+echo "export NIGHTSCOUT_HOST" >> $HOME/.bash_profile
+echo API_SECRET="$API_SECRET" >> $HOME/.bash_profile
+echo "export API_SECRET" >> $HOME/.bash_profile
 
 echo "Adding OpenAPS log shortcuts"
 oref0-log-shortcuts