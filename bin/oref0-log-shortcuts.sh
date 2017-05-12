19d555387127849e156c22a106f2ee5817d66c66
katie disimone <kdisimone@mac.com>
2017-05-11 19:25:05 -0700

    loop removed from two log aliases

--- bin/oref0-log-shortcuts.sh
+++ bin/oref0-log-shortcuts.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # add crontab entries
-grep -q network-looplog ~/.bash_profile 2>/dev/null || echo "alias network-looplog="'"tail -n 100 -F /var/log/openaps/network.log"' >> ~/.bash_profile
+grep -q networklog ~/.bash_profile 2>/dev/null || echo "alias networklog="'"tail -n 100 -F /var/log/openaps/network.log"' >> ~/.bash_profile
 grep -q xdrip-looplog ~/.bash_profile || echo "alias xdrip-looplog="'"tail -n 100 -F /var/log/openaps/xdrip-loop.log"' >> ~/.bash_profile
 grep -q cgm-looplog ~/.bash_profile || echo "alias cgm-looplog="'"tail -n 100 -F /var/log/openaps/cgm-loop.log"' >> ~/.bash_profile
 grep -q autosens-looplog ~/.bash_profile || echo "alias autosens-looplog="'"tail -n 100 -F /var/log/openaps/autosens-loop.log"' >> ~/.bash_profile
-grep -q autotune-looplog ~/.bash_profile || echo "alias autotune-looplog="'"tail -n 100 -F /var/log/openaps/autotune.log"' >> ~/.bash_profile
+grep -q autotunelog ~/.bash_profile || echo "alias autotunelog="'"tail -n 100 -F /var/log/openaps/autotune.log"' >> ~/.bash_profile
 grep -q pump-looplog ~/.bash_profile || echo "alias pump-looplog="'"tail -n 100 -F /var/log/openaps/pump-loop.log"' >> ~/.bash_profile
 grep -q urchin-looplog ~/.bash_profile || echo "alias urchin-looplog="'"tail -n 100 -F /var/log/openaps/urchin-loop.log"' >> ~/.bash_profile
 grep -q ns-looplog ~/.bash_profile || echo "alias ns-looplog="'"tail -n 100 -F /var/log/openaps/ns-loop.log"' >> ~/.bash_profile
