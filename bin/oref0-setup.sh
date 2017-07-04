ff901fc17539118b9cbd596e076ac7adcc952206
Scott Leibrand <scottleibrand@gmail.com>
2017-07-04 12:06:04 -0700

    find last BG from before a meal, and interpolate any gaps (#518)
    * find last BG from before a meal, and interpolate any gaps
    
    * log autosens ratio in pump-loop.log; better rounding
    
    * consistent y/[N] prompts

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -245,7 +245,7 @@ if [[ -z "$DIR" || -z "$serial" ]]; then
         echo
     fi
     if [[ ! -z $NIGHTSCOUT_HOST ]]; then
-        read -p "Starting with oref 0.5.0 you can use token based authentication to Nightscout. This makes it possible to deny anonymous access to your Nightscout instance. It's more secure than using your API_SECRET. Do you want to use token based authentication y/[N]?" -r
+        read -p "Starting with oref 0.5.0 you can use token based authentication to Nightscout. This makes it possible to deny anonymous access to your Nightscout instance. It's more secure than using your API_SECRET. Do you want to use token based authentication? y/[N] " -r
         if [[ $REPLY =~ ^[Yy]$ ]]; then
             read -p "What Nightscout access token (i.e. subjectname-hashof16characters) do you want to use for this rig? " -r
             API_SECRET="token=${REPLY}"
@@ -261,7 +261,7 @@ if [[ -z "$DIR" || -z "$serial" ]]; then
         fi
     fi
 
-    read -p "Do you want to be able to setup BT tethering later? y[N] " -r
+    read -p "Do you want to be able to setup BT tethering later? y/[N] " -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
     read -p "What is your phone's BT MAC address (i.e. AA:BB:CC:DD:EE:FF)? " -r
         BT_MAC=$REPLY
