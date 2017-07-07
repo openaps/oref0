6c662be72fb05555ed790a88df8f8ea27da156e5
PieterGit <pieterg@gmail.com>
2017-07-07 15:48:25 +0200

    Reboot rig if Unable to initialize libusb: -99 errors with Dexcom connected to G4 and uptime > 30m
    Fixes https://github.com/openaps/oref0/issues/527
    
    If there is no CGM for more than OLD_LSUSB minutes (default 16)
    and the lsusb is failing (e.g. unable to initialize libusb: -99)
    and uptime is more than 30 minutes
    reboot the system to restore the USB subsystem and the CGM
    that is connected to USB OTG
    
    Example output:
    ```
    root@edison:~/myopenaps-cgm-loop# openaps monitor-cgm
    CGM Data 17.51 mins ago is old (>=5.1), not waiting
    unable to initialize libusb: -99
    CGM Data 17.51 mins ago is old (>=16.0) and lsusb returns error:
    unable to initialize libusb: -99
    Uptime:  32.1483  minutes
    <system will reboot>
    ```

--- bin/oref0-dex-is-fresh.sh
+++ bin/oref0-dex-is-fresh.sh
@@ -4,9 +4,25 @@ GLUCOSE=$1
 
 OLD=${2-5}
 TIME_SINCE=$(oref0-dex-time-since $GLUCOSE)
+OLD_LSUSB=16.0
 
 if (( $(bc <<< "$TIME_SINCE >= $OLD") )); then
   echo "CGM Data $TIME_SINCE mins ago is old (>=$OLD)"
+ 
+  # if there is no CGM for more than OLD_LSUSB minutes
+  # and uptime is more than 30 minutes
+  # reboot the system to restore the USB subsystem and the CGM
+  # that is connected to USB OTG
+  if (( $(bc <<< "$TIME_SINCE >= $OLD_LSUSB") )); then
+     if ! lsusb > /dev/null ; then
+        echo "CGM Data $TIME_SINCE mins ago is old (>=$OLD_LSUSB) and lsusb returns error: "
+        lsusb
+        if ! awk '{print "Uptime: ", $0/60, " minutes"; if ($0/60 > 30) exit 1;}' /proc/uptime  ; then
+                shutdown -r now "lsusb error. Rebooting to restore USB subsystem"
+        fi
+     fi
+  fi
+  
   exit 1
 else
   echo "CGM Data $TIME_SINCE mins ago is fresh (< $OLD)"
