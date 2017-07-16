1943408ecd2a608c591ac71dbabaa0b6d601409b
Scott <scottleibrand@gmail.com>
2017-07-16 11:48:34 -0700

    import openaps install scripts from docs

---
+++ bin/openaps-bootstrap.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+(
+dmesg -D
+echo Scanning for wifi networks:
+ifup wlan0
+wpa_cli scan
+echo -e "\nStrongest networks found:"
+wpa_cli scan_res | sort -grk 3 | head | awk -F '\t' '{print $NF}' | uniq
+set -e
+echo -e /"\nWARNING: this script will back up and remove all of your current wifi configs."
+read -p "Press Ctrl-C to cancel, or press Enter to continue:" -r
+echo -e "\nNOTE: Spaces in your network name or password are ok. Do not add quotes."
+read -p "Enter your network name: " -r
+SSID=$REPLY
+read -p "Enter your network password: " -r
+PSK=$REPLY
+cd /etc/network
+cp interfaces interfaces.$(date +%s).bak
+echo -e "auto lo\niface lo inet loopback\n\nauto usb0\niface usb0 inet static\n  address 10.11.12.13\n  netmask 255.255.255.0\n\nauto wlan0\niface wlan0 inet dhcp\n  wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" > interfaces
+echo -e "\n/etc/network/interfaces:\n"
+cat interfaces
+cd /etc/wpa_supplicant/
+cp wpa_supplicant.conf wpa_supplicant.conf.$(date +%s).bak
+echo -e "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\nnetwork={\n  ssid=\"$SSID\"\n  psk=\"$PSK\"\n}" > wpa_supplicant.conf
+echo -e "\n/etc/wpa_supplicant/wpa_supplicant.conf:\n"
+cat wpa_supplicant.conf
+echo -e "\nAttempting to bring up wlan0:\n"
+ifdown wlan0; ifup wlan0
+sleep 10
+echo -ne "\nWifi SSID: "; iwgetid -r
+sleep 5
+# TODO check for options to fix the certificate activation error message for https
+cd /tmp/; wget --no-check-certificate https://raw.githubusercontent.com/openaps/docs/dev/scripts/openaps-install.sh; bash ./openaps-install.sh
+)
