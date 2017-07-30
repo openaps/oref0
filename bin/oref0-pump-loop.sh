95469984a2a99f3116c8a31dd59ce326a6854c93
Scott <scottleibrand@gmail.com>
2017-07-30 01:08:33 -0700

    print any_pump_comms output when it doesn't succeed

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -361,12 +361,12 @@ function wait_for_silence {
         waitfor=$1
     fi
     # check radio multiple times, and mmtune if all checks fail
-    ( ( any_pump_comms 1 | grep -qi comms ) || \
-      ( echo -n .; sleep 1; any_pump_comms 1 | grep -qi comms ) || \
-      ( echo -n .; sleep 2; any_pump_comms 1 | grep -qi comms ) || \
-      ( echo -n .; sleep 4; any_pump_comms 1 | grep -qi comms ) || \
-      ( echo -n .; sleep 8; any_pump_comms 1 | grep -qi comms ) \
-    ) 2>&1 | tail -1 \
+    ( ( out=$(any_pump_comms 1) && echo $out | grep -qi comms || (echo $out; false) ) || \
+      ( echo -n .; sleep 1; out=$(any_pump_comms 1) && echo $out | grep -qi comms || (echo $out; false) ) || \
+      ( echo -n .; sleep 2; out=$(any_pump_comms 1) && echo $out | grep -qi comms || (echo $out; false) ) || \
+      ( echo -n .; sleep 4; out=$(any_pump_comms 1) && echo $out | grep -qi comms || (echo $out; false) ) || \
+      ( echo -n .; sleep 8; out=$(any_pump_comms 1) && echo $out | grep -qi comms || (echo $out; false) )
+    ) 2>&1 | tail -5 \
         && echo -n "Radio ok. " || (echo -n "Radio check failed. "; any_pump_comms 1 2>&1 | tail -1; mmtune)
     echo -n "Listening: "
     for i in $(seq 1 800); do
