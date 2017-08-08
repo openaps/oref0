6f615bcf7e30f6a90bef80903f02627d54d7f666
Sulka Haro <sulka@sulka.net>
2017-08-08 17:02:36 +0300

    Testing relative require paths in case that makes the server happy, fixed formatting

--- tests/cob.test.js
+++ tests/cob.test.js
@@ -7,25 +7,27 @@ describe('cobhistory', function() {
     var total_cob = require('../lib/meal/total');
 
     var ph = [{
-        "_type": "Rewind",
-        "timestamp": "2016-06-19T10:59:36+03:00"
-    },
-        {
-            "_type": "Bolus",
-            "timestamp": "2016-06-19T12:59:36+03:00",
-            "amount": 5,
-            "bolus": 5}];
-
-    var treatments = [{
-            "_type": "BolusWizard",
-            "timestamp": "2016-06-19T12:59:36+03:00",
-            "carbs": 40
+            '_type': 'Rewind',
+            'timestamp': '2016-06-19T10:59:36+03:00'
         },
         {
-            "_type": "Bolus",
-            "timestamp": "2016-06-19T12:59:36+03:00",
-            "amount": 5,
-            "bolus": 5
+            '_type': 'Bolus',
+            'timestamp': '2016-06-19T12:59:36+03:00',
+            'amount': 5,
+            'bolus': 5
+        }
+    ];
+
+    var treatments = [{
+            '_type': 'BolusWizard',
+            'timestamp': '2016-06-19T12:59:36+03:00',
+            'carbs': 40
+        },
+        {
+            '_type': 'Bolus',
+            'timestamp': '2016-06-19T12:59:36+03:00',
+            'amount': 5,
+            'bolus': 5
         }
     ];
 
@@ -58,35 +60,35 @@ describe('cobhistory', function() {
             autosens_max: 1.15,
             autosens_min: 0.85,
             carb_ratios: {
-                units: "grams",
+                units: 'grams',
                 first: 1,
                 schedule: [{
-                    "q": 0,
-                    "start": "00:00:00",
-                    "r": 140,
-                    "ratio": 10,
-                    "offset": 0,
-                    "i": 0,
-                    "x": 0
+                    'q': 0,
+                    'start': '00:00:00',
+                    'r': 140,
+                    'ratio': 10,
+                    'offset': 0,
+                    'i': 0,
+                    'x': 0
                 }]
             },
             min_5m_carbimpact: 5,
             isfProfile: {
-                units: "mg/dL",
-                user_preferred_units: "mmol/L",
+                units: 'mg/dL',
+                user_preferred_units: 'mmol/L',
                 sensitivities: [{
-                    "i": 0,
-                    "start": "00:00:00",
-                    "sensitivity": 115,
-                    "x": 0,
-                    "offset": 0
+                    'i': 0,
+                    'start': '00:00:00',
+                    'sensitivity': 115,
+                    'x': 0,
+                    'offset': 0
                 }],
-                "first": 2
+                'first': 2
             }
         };
 
-        var startTime = new Date("2016-06-19T12:00:36+03:00");
-        var time = new Date("2016-06-19T12:59:36+03:00");
+        var startTime = new Date('2016-06-19T12:00:36+03:00');
+        var time = new Date('2016-06-19T12:59:36+03:00');
         var glucose = [];
 
         for (var i = 0; i < 13; i++) {
@@ -105,12 +107,12 @@ describe('cobhistory', function() {
         console.log(output);
         output.mealCOB.should.equal(40);
 
-        time = new Date("2016-06-19T13:59:36+03:00");
+        time = new Date('2016-06-19T13:59:36+03:00');
         glucose = [];
 
         for (var i = 0; i < 24; i++) {
-            var entry = {};
-            var d = new Date(startTime.getTime() + (i * 5 * 60 * 1000));
+            entry = {};
+            d = new Date(startTime.getTime() + (i * 5 * 60 * 1000));
             entry.dateString = d.toISOString();
             entry.date = d.getTime();
             entry.sgv = 100 + i * 10;
