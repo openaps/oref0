718959e500368b9509302f269de46095265ba4e7
Scott <scottleibrand@gmail.com>
2017-09-04 11:26:17 -0700

    Revert "remove -g as it breaks autotune's time filters"
    This reverts commit b60f34123eca201a066a11aa3420b02b43dfd6d7.

--- bin/ns-get.sh
+++ bin/ns-get.sh
@@ -8,7 +8,7 @@ NIGHTSCOUT_HOST=${NIGHTSCOUT_HOST-${2-localhost:1337}}
 QUERY=${3}
 OUTPUT=${4-/dev/fd/1}
 
-CURL_FLAGS="--compressed -s"
+CURL_FLAGS="--compressed -g -s"
 NIGHTSCOUT_FORMAT=${NIGHTSCOUT_FORMAT-json}
 test "$NIGHTSCOUT_DEBUG" = "1" && CURL_FLAGS="${CURL_FLAGS} -iv"
 test "$NIGHTSCOUT_DEBUG" = "1" && set -x
