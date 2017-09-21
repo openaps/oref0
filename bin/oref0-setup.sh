89e120e3a19b4ac7514e979105e549897221769b
Dana Lewis <dmlewis@ua.edu>
2017-09-20 17:27:14 -0700

    First attempt at scripting file copying and alias changes automatically for x12 pump models

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -1025,6 +1025,15 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo "To pair your G4 Share receiver, open its Settings, select Share, Forget Device (if previously paired), then turn sharing On"
     fi
 
+    if [[ ${pumpmodel,,} =~ "x12" ]]; then
+     echo "copying settings files for x12 pumps"
+        cd ~/myopenaps && cp ./raw-pump/bg-targets-raw.json ./settings/ && cp ./raw-pump/selected-basal-profile.json ./settings/ && cp ./raw-pump/settings.json ./settings/ 
+     echo "getting ready to remove get-settings since this is an x12"
+        openaps alias remove get-settings 
+     echo "settings removed, getting ready to add x12 settings"
+        openaps alias add get-settings "report invoke settings/model.json settings/bg_targets.json settings/insulin_sensitivities_raw.json settings/insulin_sensitivities.json settings/carb_ratios.json settings/profile.json"
+    fi
+
 
 fi # from 'read -p "Continue? y/[N] " -r' after interactive setup is complete
 
