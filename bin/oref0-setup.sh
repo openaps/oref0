65a549778be40764d13aa209fb5762fa2bda0aaa
Scott <scottleibrand@gmail.com>
2017-09-21 16:57:18 -0700

    don't ask about SMB for x12 pumps

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -415,7 +415,7 @@ if [[ ${CGM,,} =~ "shareble" ]]; then
 fi
 echo
 echo -n "NS host $NIGHTSCOUT_HOST, "
-if [[ ! -z "$pumpmodel" ]]; then
+if [[ ${pumpmodel,,} =~ "x12" ]]; then
     echo -n "x12 pump, "
 fi
 
@@ -922,20 +922,6 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         done
     fi
 
-    # configure supermicrobolus if enabled
-    # WARNING: supermicrobolus mode is not yet documented or ready for general testing
-    # It should only be tested with a disconnected pump not administering insulin.
-    # If you aren't sure what you're doing, *DO NOT* enable this.
-    # If you ignore this warning, it *WILL* administer extra post-meal insulin, which may cause low blood sugar.
-    if [[ $ENABLE =~ microbolus ]]; then
-        sudo apt-get -y install bc jq
-        cd $directory || die "Can't cd $directory"
-        for type in supermicrobolus; do
-        echo importing $type file
-        cat $HOME/src/oref0/lib/oref0-setup/$type.json | openaps import || die "Could not import $type.json"
-        done
-    fi
-
     if [[ ${pumpmodel,,} =~ "x12" ]]; then
         echo "copying settings files for x12 pumps"
         cp $HOME/src/oref0/lib/oref0-setup/bg_targets_raw.json $directory/settings/ && cp $HOME/src/oref0/lib/oref0-setup/basal_profile.json $directory/settings/ && cp $HOME/src/oref0/lib/oref0-setup/settings.json $directory/settings/ || die "Could not copy settings files for x12 pumps"
@@ -943,6 +929,20 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         openaps alias remove get-settings || die "Could not remove get-settings"
         echo "settings removed, getting ready to add x12 settings"
         openaps alias add get-settings "report invoke settings/model.json settings/bg_targets.json settings/insulin_sensitivities_raw.json settings/insulin_sensitivities.json settings/carb_ratios.json settings/profile.json" || die "Could not add x12 settings"
+    else
+        # configure supermicrobolus if enabled
+        # WARNING: supermicrobolus mode is not yet documented or ready for general testing
+        # It should only be tested with a disconnected pump not administering insulin.
+        # If you aren't sure what you're doing, *DO NOT* enable this.
+        # If you ignore this warning, it *WILL* administer extra post-meal insulin, which may cause low blood sugar.
+        if [[ $ENABLE =~ microbolus ]]; then
+            sudo apt-get -y install bc jq
+            cd $directory || die "Can't cd $directory"
+            for type in supermicrobolus; do
+            echo importing $type file
+            cat $HOME/src/oref0/lib/oref0-setup/$type.json | openaps import || die "Could not import $type.json"
+            done
+        fi
     fi
 
     echo "Adding OpenAPS log shortcuts"
