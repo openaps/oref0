87d98b0aa54117b15c905b5cf89607801d29b8c4
Scott <scottleibrand@gmail.com>
2017-10-01 15:03:50 -0700

    use the version of openaps-install.sh in oref0 master

--- bin/openaps-bootstrap.sh
+++ bin/openaps-bootstrap.sh
@@ -29,6 +29,6 @@ ifdown wlan0; ifup wlan0
 sleep 10
 echo -ne "\nWifi SSID: "; iwgetid -r
 sleep 5
-curl https://raw.githubusercontent.com/openaps/docs/dev/scripts/openaps-install.sh > /tmp/openaps-install.sh
+curl https://raw.githubusercontent.com/openaps/oref0/master/bin/openaps-install.sh > /tmp/openaps-install.sh
 bash /tmp/openaps-install.sh
 )
