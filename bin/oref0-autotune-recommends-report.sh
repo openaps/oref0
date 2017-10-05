cba195619f6b71334dc1e155f4dc9223dbf6fb59
Dana Lewis <danamlewis@users.noreply.github.com>
2017-10-05 12:07:47 -0700

    Attempting to apply 0.6.0-dev autotune changes for patch (autotune-de-biasing)

--- bin/oref0-autotune-recommends-report.sh
+++ bin/oref0-autotune-recommends-report.sh
@@ -64,20 +64,20 @@ csf_new=$(cat $directory/autotune/profile.json | jq '.csf')
 carb_ratio_new=$(cat $directory/autotune/profile.json | jq '.carb_ratio')
 
 # Print Header Info
-printf "%-${parameter_width}s| %-${data_width}s| %-${data_width}s\n" "Parameter" "Current" "Autotune" >> $report_file
+printf "%-${parameter_width}s| %-${data_width}s| %-${data_width}s\n" "Parameter" "Pump" "Autotune" >> $report_file
 printf "%s\n" "-------------------------------------" >> $report_file
 
 # Print ISF, CSF and Carb Ratio Recommendations
 printf "%-${parameter_width}s| %-${data_width}.3f| %-${data_width}.3f\n" "ISF [mg/dL/U]" $isf_current $isf_new >> $report_file
-if [ $csf_current != null ]; then
-  printf "%-${parameter_width}s| %-${data_width}.3f| %-${data_width}.3f\n" "CSF [mg/dL/g]" $csf_current $csf_new >> $report_file
-else
-  printf "%-${parameter_width}s| %-${data_width}s| %-${data_width}.3f\n" "CSF [mg/dL/g]" "n/a" $csf_new >> $report_file
-fi
-printf "%-${parameter_width}s| %-${data_width}.3f| %-${data_width}.3f\n" "Carb Ratio [g]" $carb_ratio_current $carb_ratio_new >> $report_file
+# if [ $csf_current != null ]; then
+  # printf "%-${parameter_width}s| %-${data_width}.3f| %-${data_width}.3f\n" "CSF [mg/dL/g]" $csf_current $csf_new >> $report_file
+# else
+  # printf "%-${parameter_width}s| %-${data_width}s| %-${data_width}.3f\n" "CSF [mg/dL/g]" "n/a" $csf_new >> $report_file
+# fi
+printf "%-${parameter_width}s| %-${data_width}.3f| %-${data_width}.3f\n" "Carb Ratio[g/U]" $carb_ratio_current $carb_ratio_new >> $report_file
 
 # Print Basal Profile Recommendations
-printf "%-${parameter_width}s| %-${data_width}s|\n" "Basal Profile  [unit/hour]" "-" >> $report_file
+printf "%-${parameter_width}s| %-${data_width}s|\n" "Basals [U/hr]" "-" >> $report_file
 
 # Build time_list array of H:M in 30 minute increments to mirror pump basal schedule
 time_list=()
