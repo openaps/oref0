9a4f9228037abbc804f3ef2822d5d5475726bdf4
Scott <scottleibrand@gmail.com>
2017-10-06 18:00:39 -0700

    require enact/smb-suggested.json be <5m old instead

--- bin/oref0-pushover.sh
+++ bin/oref0-pushover.sh
@@ -16,12 +16,10 @@ fi
 date
 if find monitor/ -mmin -$SNOOZE | grep -q pushover-sent; then
     echo "Last pushover sent less than $SNOOZE minutes ago."
-elif ! find $FILE -mmin -$SNOOZE | grep -q $FILE; then
-    echo "$FILE more than $SNOOZE minutes old"
+elif ! find $FILE -mmin -5 | grep -q $FILE; then
+    echo "$FILE more than 5 minutes old"
 elif ! cat $FILE | egrep "add'l|maxBolus"; then
     echo "No additional carbs or bolus required."
 else
     curl -s -F "token=$TOKEN" -F "user=$USER" -F "sound=$SOUND" -F "message=$(jq -c "{bg, tick, carbsReq, insulinReq, reason}|del(.[] | nulls)" $FILE) - $(hostname)" https://api.pushover.net/1/messages.json && touch monitor/pushover-sent
-    # delete smb-suggested.json to avoid duplicate notifications if this rig can't loop for 15m
-    rm $FILE
 fi
