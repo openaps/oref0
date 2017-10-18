a86eeb9bb313123323cc1c88ca3cc4c46bf03b22
Scott <scottleibrand@gmail.com>
2017-10-17 23:52:05 -0700

    allow oref0-append-local-temptarget.sh to run oref0-set-local-temptarget

--- bin/oref0-append-local-temptarget.sh
+++ bin/oref0-append-local-temptarget.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cat "${1:-/dev/stdin}" \
+if [[ ! -z "$2" ]]; then
+    input=$(oref0-set-local-temptarget $@)
+elif [[ ! -z "$1" ]]; then
+    input=$(cat $1)
+else
+    input=$(cat /dev/stdin)
+fi
+#cat "${1:-/dev/stdin}" \
+echo $input \
     | tee /tmp/temptarget.json \
     && jq -s '[.[0]] + .[1]' /tmp/temptarget.json settings/local-temptargets.json \
     | tee settings/local-temptargets.json.new \
