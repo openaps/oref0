936e5d0c825fa743bc217844c7b6719f3cf3c3d6
Scott <scottleibrand@gmail.com>
2017-10-18 19:12:43 -0700

    jq pretty colors

--- bin/oref0-ns-loop.sh
+++ bin/oref0-ns-loop.sh
@@ -99,8 +99,7 @@ function upload_recent_treatments {
     #echo Uploading treatments
     format_latest_nightscout_treatments || die "Couldn't format latest NS treatments"
     if test $(json -f upload/latest-treatments.json -a created_at eventType | wc -l ) -gt 0; then
-        ns-upload $NIGHTSCOUT_HOST $API_SECRET treatments.json upload/latest-treatments.json || die "Couldn't upload latest treatments to NS"
-        echo ed successfully
+        ns-upload $NIGHTSCOUT_HOST $API_SECRET treatments.json upload/latest-treatments.json | jq -C -c . || die "Couldn't upload latest treatments to NS"
     else
         echo "No recent treatments to upload"
     fi
