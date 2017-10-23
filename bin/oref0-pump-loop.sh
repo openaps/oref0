e6af2690882c87f12942cb9d050f2d6028058411
Scott <scottleibrand@gmail.com>
2017-10-22 17:57:32 -0700

    only check profile.json not iob.json in refresh_old_profile

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -492,15 +492,14 @@ function refresh_old_pumphistory_24h {
 function refresh_old_profile {
     find settings/ -mmin -60 -size +5c | grep -q settings/profile.json && echo -n "Profile less than 60m old. " \
         || (echo -n Old settings refresh && openaps get-settings 2>&1 >/dev/null | tail -1 && echo -n "ed. " )
-    if ! cat monitor/iob.json | jq . | grep -q iob || ! cat settings/profile.json | jq . | grep -q basal; then
-        if ! cat monitor/iob.json | jq . | grep -q iob; then
-            echo -n "Invalid iob.json: "
-        fi
+    #if ! cat monitor/iob.json | jq . | grep -q iob || ! cat settings/profile.json | jq . | grep -q basal; then
+        #if ! cat monitor/iob.json | jq . | grep -q iob; then
+            #echo -n "Invalid iob.json: "
+        #fi
         if ! cat settings/profile.json | jq . | grep -q basal; then
-            echo -n "Invalid profile.json: "
+            echo -n "Invalid profile.json: refresh" && openaps get-settings 2>&1 >/dev/null | tail -1 && echo -n "ed. "
         fi
-        echo -n refresh && openaps get-settings 2>&1 >/dev/null | tail -1 && echo -n "ed. "
-    fi
+    #fi
 }
 
 function refresh_smb_temp_and_enact {
