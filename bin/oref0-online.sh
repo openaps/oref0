012f5d1ec1ebd4c83d29a867553727946afc5cac
Scott <scottleibrand@gmail.com>
2017-10-30 20:31:00 -0700

    fix return code

--- bin/oref0-online.sh
+++ bin/oref0-online.sh
@@ -97,6 +97,7 @@ function check_ip {
     PUBLIC_IP=$(curl --compressed -4 -s -m 15 checkip.amazonaws.com | awk -F , '{print $NF}' | egrep "^[12]*[0-9]*[0-9]\.[12]*[0-9]*[0-9]\.[12]*[0-9]*[0-9]\.[12]*[0-9]*[0-9]$")
     if [[ -z $PUBLIC_IP ]]; then
         echo not found
+        return 1
     else
         echo $PUBLIC_IP
     fi
