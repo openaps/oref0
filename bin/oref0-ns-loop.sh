06c1b0c28098e2f1954e0912f9fc75e688645d14
Scott <scottleibrand@gmail.com>
2017-10-31 22:41:36 -0700

    logging

--- bin/oref0-ns-loop.sh
+++ bin/oref0-ns-loop.sh
@@ -176,7 +176,7 @@ function autosens {
     if find settings/ -newer settings/autosens.json | grep -q pumphistory-24h-zoned.json \
         || find settings/ -size -5c | grep -q autosens.json \
         || ! find settings/ | grep -q autosens \
-        || ! find settings/autosens.json; then
+        || ! find settings/autosens.json >/dev/null; then
         if oref0-detect-sensitivity monitor/glucose.json settings/pumphistory-24h-zoned.json settings/insulin_sensitivities.json settings/basal_profile.json settings/profile.json monitor/carbhistory.json settings/temptargets.json > settings/autosens.json.new && cat settings/autosens.json.new | jq .ratio | grep -q [0-9]; then
             mv settings/autosens.json.new settings/autosens.json
             echo -n "Autosens refreshed: "
