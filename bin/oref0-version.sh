158500e0cf21649f63064cd9a95506ad60099a8a
Scott <scottleibrand@gmail.com>
2017-10-31 18:18:55 -0700

    tabs to spaces

--- bin/oref0-version.sh
+++ bin/oref0-version.sh
@@ -6,21 +6,21 @@ branch=$(cd $HOME/src/oref0/ && git rev-parse --abbrev-ref HEAD)
 version=$(jq .version "$HOME/src/oref0/package.json" | sed 's/"//g')
 
 if [[ $1 =~ "update" ]]; then
-	cd $HOME/src/oref0/ && timeout 30 git fetch 2>/dev/null || echo git fetch failed # pull latest remote info
-	behind=$(cd $HOME/src/oref0/ && git rev-list --count ${branch}...origin/${branch})
+    cd $HOME/src/oref0/ && timeout 30 git fetch 2>/dev/null || echo git fetch failed # pull latest remote info
+    behind=$(cd $HOME/src/oref0/ && git rev-list --count ${branch}...origin/${branch})
     if (("$behind" > "0")); then
-		# we are out of date
-		echo "Your instance of oref0 [${version}, ${branch}] is out-of-date by ${behind} commits: you may want to consider updating."
+        # we are out of date
+        echo "Your instance of oref0 [${version}, ${branch}] is out-of-date by ${behind} commits: you may want to consider updating."
         if [ $branch != "master" ]; then
-			echo "You are currently running a development branch of oref0.  Such branches change frequently."
-			echo "Please read the latest PR notes and update with the latest commits to dev before reporting any issues."
-		else
-			echo "Please make sure to read any new documentation and release notes that accompany the update."
-		fi
-	else
-		echo "Your instance of oref0 [${version}, ${branch}] is up-to-date."
-	fi
+            echo "You are currently running a development branch of oref0.  Such branches change frequently."
+            echo "Please read the latest PR notes and update with the latest commits to dev before reporting any issues."
+        else
+            echo "Please make sure to read any new documentation and release notes that accompany the update."
+        fi
+    else
+        echo "Your instance of oref0 [${version}, ${branch}] is up-to-date."
+    fi
 else
-	# simple version check and report.
-	echo "${version} [${branch}]"
+    # simple version check and report.
+    echo "${version} [${branch}]"
 fi
