1e55c5a823dc915ccca7499cdf5ba119f90c2303
kkota1 <kiranmkota@gmail.com>
2017-11-27 13:39:16 -0700

    js cleanup start changed splice/pop to array element cleaned up ns-status

--- bin/ns-status.js
+++ bin/ns-status.js
@@ -24,30 +24,26 @@ var requireUtils = require('../lib/require-utils'),
 */
 
 function mmtuneStatus (status) {
-    if (mmtune_input) {
-        var mmtune = requireWithTimestamp(cwd + '/' + mmtune_input);
-        if (mmtune) {
-            if (mmtune.scanDetails && mmtune.scanDetails.length) {
-                mmtune.scanDetails = mmtune.scanDetails.filter(function (d) {
-                    return d[2] > -99;
-                });
-            }
-          status.mmtune = mmtune;
+    var mmtune = requireWithTimestamp(cwd + mmtune_input);
+    if (mmtune) {
+        if (mmtune.scanDetails && mmtune.scanDetails.length) {
+            mmtune.scanDetails = mmtune.scanDetails.filter(function (d) {
+                return d[2] > -99;
+            });
         }
+      status.mmtune = mmtune;
     }
 }
 
 function uploaderStatus (status) {
-    if (uploader_input ) {
-        var uploader = require(cwd + '/' + uploader_input);
-        if (uploader) {
-            if (typeof uploader === 'number') {
-                status.uploader = {
-                    battery: uploader
-                };
-            } else {
-                status.uploader = uploader;
-            }
+    var uploader = require(cwd + uploader_input);
+    if (uploader) {
+        if (typeof uploader === 'number') {
+            status.uploader = {
+                battery: uploader
+            };
+        } else {
+            status.uploader = uploader;
         }
     }
 }
@@ -64,22 +60,21 @@ if (!module.parent) {
         .strict(true)
         .help('help');
 
-    var params = argv.argv;
+    var params = argv.argv,
+        inputs = params._,
+        clock_input = inputs[0],
+        iob_input = inputs[1],
+        suggested_input = inputs[2],
+        enacted_input = inputs[3],
+        battery_input = inputs[4],
+        reservoir_input = inputs[5],
+        status_input = inputs[6],
+        mmtune_input = inputs[7],
+        uploader_input = params.uploader;
 
-
-    var clock_input = params._.slice(0, 1).pop();
-    var iob_input = params._.slice(1, 2).pop();
-    var suggested_input = params._.slice(2, 3).pop();
-    var enacted_input = params._.slice(3, 4).pop();
-    var battery_input = params._.slice(4, 5).pop();
-    var reservoir_input = params._.slice(5, 6).pop();
-    var status_input = params._.slice(6, 7).pop();
-    var mmtune_input = params._.slice(7, 8).pop();
-    var uploader_input = params.uploader;
-
-    if (params._.length > 8) {
-        uploader_input = params.uploader ? params._.slice(7, 8).pop() : false;
-        mmtune_input = params._.slice(8, 9).pop();
+    if (inputs.length > 8) {
+        uploader_input = params.uploader ? inputs[7] : false;
+        mmtune_input = inputs[8];
     }
 
     if (!clock_input || !iob_input || !suggested_input || !enacted_input || !battery_input || !reservoir_input || !status_input) {
@@ -87,7 +82,7 @@ if (!module.parent) {
         process.exit(1);
     }
 
-    var cwd = process.cwd();
+    var cwd = process.cwd() + '/';
 
     var hostname = 'unknown';
     try {
@@ -97,12 +92,12 @@ if (!module.parent) {
     }
 
     try {
-        var iob = null
-          , iobArray = requireWithTimestamp(cwd + '/' + iob_input)
-          , suggested = requireWithTimestamp(cwd + '/' + suggested_input)
-          , enacted = requireWithTimestamp(cwd + '/' + enacted_input);
+        var iob = null,
+            iobArray = requireWithTimestamp(cwd + iob_input),
+            suggested = requireWithTimestamp(cwd + suggested_input),
+            enacted = requireWithTimestamp(cwd + enacted_input);
 
-        if (iobArray && iobArray.length && iobArray.length > 0) {
+        if (iobArray && iobArray.length) {
             iob = iobArray[0];
             iob.timestamp = iob.time;
             delete iob.time;
@@ -118,25 +113,30 @@ if (!module.parent) {
         }
 
         var status = {
-            device: 'openaps://' + os.hostname()
-            , openaps: {
-                iob: iob
-                , suggested: suggested
-                , enacted: enacted
-            }
-            , pump: {
-                clock: safeRequire(cwd + '/' + clock_input)
-                , battery: safeRequire(cwd + '/' + battery_input)
-                , reservoir: safeRequire(cwd + '/' + reservoir_input)
-                , status: requireWithTimestamp(cwd + '/' + status_input)
+            device: 'openaps://' + os.hostname(),
+            openaps: {
+                iob: iob,
+                suggested: suggested,
+                enacted: enacted
+            },
+            pump: {
+                clock: safeRequire(cwd + clock_input),
+                battery: safeRequire(cwd + battery_input),
+                reservoir: safeRequire(cwd + reservoir_input),
+                status: requireWithTimestamp(cwd + status_input)
             }
         };
 
-        mmtuneStatus(status);
-        uploaderStatus(status);
+        if (mmtune_input) {
+            mmtuneStatus(status);
+        }
+
+        if (uploader_input) {
+            uploaderStatus(status);
+        }
+
+        console.log(JSON.stringify(status));
     } catch (e) {
         return console.error("Could not parse input data: ", e);
     }
-
-    console.log(JSON.stringify(status));
 }
