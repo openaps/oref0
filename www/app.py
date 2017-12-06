8c1553e63a37f707581cb56255216c14a4b6724a
fidoman <26698840+efidoman@users.noreply.github.com>
2017-12-06 18:43:31 -0500

    change environment variable to OPENAPS_DIR

--- www/app.py
+++ www/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     try:
-      myopenaps_dir = os.environ['OPENAPSDIR']
+      myopenaps_dir = os.environ['OPENAPS_DIR']
     except KeyError:
         myopenaps_dir = "/root/myopenaps/"
     data=dict()
