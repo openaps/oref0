7693a0286c00e57948d21e7b33a02dac249a2468
fidoman <26698840+efidoman@users.noreply.github.com>
2017-12-10 19:27:11 -0500

    refactor for simplicity performance plus ifttt
    Refactored for simplicity and performance to just read from enact/smb-suggested.json
    
    Also, added another optional notification that will send an ifttt maker event "carbs-required" if additional carbs are required and if environment variable "MAKER_KEY" is set. This is the ifttt webhooks Maker key https://ifttt.com/maker_webhooks
    The "carbs-required" event can then be used with a ifttt recipe to perform an action based on carbs required. A good use case is to use the ifttt phone action to get a phone call with this event that will read out in human language the additional carbs and other vital facts. It will leave a voice mail if not answered

--- bin/oref0-pushover.sh
+++ bin/oref0-pushover.sh
@@ -29,54 +29,41 @@ else
     curl -s -F "token=$TOKEN" -F "user=$USER" -F "sound=$SOUND" -F "message=$(jq -c "{bg, tick, carbsReq, insulinReq, reason}|del(.[] | nulls)" $FILE) - $(hostname)" https://api.pushover.net/1/messages.json && touch monitor/pushover-sent
 fi
 
-# Enhance to send to pushover glances API
+# Send to pushover glances API 
 #   For watch face updates once every 10 minutes
-#   Works for apple watch complications
+#   Works for apple watch complications 
 #   Not tested fo android wear notifications yet
 
 # Use file to make sure we don't update glances more often than every 10 mins
 # Apple watch disables any complication updates that happen more frequently
 
+source $HOME/.bash_profile
+key=${MAKER_KEY:-"null"}
+carbsReq=`jq .carbsReq ${FILE}`
+tick=`jq .tick ${FILE}`
+bgNow=`jq .bg ${FILE}`
+delta=${tick}
+cob=`jq .COB $FILE`
+iob=`jq .IOB $FILE`
+
+#echo "carbsReq=${carbsReq} tick=${tick} bgNow=${bgNow} delta=${delta} cob=${cob} iob=${iob}"
 pushoverGlances=$(cat preferences.json | jq -M '.pushoverGlances')
 
 if [ "${pushoverGlances}" == "null" -o "${pushoverGlances}" == "false" ]; then
     echo "No preference or false preference for pushoverGlances"
-  exit
-fi
-
-
-GLANCES="monitor/last_glance"
-GLUCOSE="monitor/glucose.json"
-if [ ! -f $GLANCES ]; then
-  # First time through it will get created older than 10 minutes so it'll fire
-  touch $GLANCES && touch -r $GLANCES -d '-11 mins' $GLANCES
-fi
-
-if test `find $GLANCES -mmin +10`
-then
-  BAT="monitor/edison-battery.json"
-  carbsReq=`jq .carbsReq $FILE`
-  bgNow=$(cat $GLUCOSE | jq -M '.[0].glucose')
-  bgLast=$(cat $GLUCOSE | jq -M '.[1].glucose')
-  COB=`jq .COB $FILE`
-
-  IOB=$(cat $FILE | jq -M '.IOB')
-  IOB="${IOB%\"}"
-  IOB="${IOB#\"}"
-  enactTime=$(ls -l  --time-style=+"%l:%M" ${FILE} | awk '{printf ($6)}')
-  battery=0
-  if [ -e $BAT ]; then
-    battery=$(cat $BAT | jq -M '.battery')
-    battery="${battery%\"}"
-    battery="${battery#\"}"
+else
+  GLANCES="monitor/last_glance"
+  GLUCOSE="monitor/glucose.json"
+  if [ ! -f $GLANCES ]; then
+    # First time through it will get created older than 10 minutes so it'll fire
+    touch $GLANCES && touch -r $GLANCES -d '-11 mins' $GLANCES
   fi
 
+  if test `find $GLANCES -mmin +10`
+  then
+    enactTime=$(ls -l  --time-style=+"%l:%M" ${FILE} | awk '{printf ($6)}')
 
-
-  direction=""
-  if [ ${bgLast} -gt 0 ]; then
-    delta=$(expr ${bgNow} - ${bgLast})
-
+    direction=""
     if [ ${delta} -gt 8 ]; then
       direction="++"
     elif [ ${delta} -gt 3 ]; then
@@ -88,16 +75,71 @@ then
     else
       direction="--"
     fi
-  fi
-  if [ test cat $FILE | egrep "add'l" ]; then  
-    subtext="cr ${carbsReq}g"
-  else
-    subtext="e${enactTime}"
-  fi
-  text="${bgNow}${direction}"
-  title="cob ${COB}, iob ${IOB}"
+    if [ test cat $FILE | egrep "add'l" ]; then
+      subtext="cr ${carbsReq}g"
+    else
+      subtext="e${enactTime}"
+    fi
+    text="${bgNow}${direction}"
+    title="cob ${cob}, iob ${iob}"
 
-  echo "pushover glance text=${text}  subtext=${subtext}  delta=${delta} bgLast=${bgLast}  title=${title}  battery percent=${battery}"
-  touch $GLANCES
-  curl -s -F "token=$TOKEN" -F "user=$USER" -F "text=${text}" -F "subtext=${subtext}" -F "count=$bgNow" -F "percent=${battery}" -F "title=${title}"   https://api.pushover.net/1/glances.json
+#    echo "pushover glance text=${text}  subtext=${subtext}  delta=${delta} title=${title}  battery percent=${battery}"
+    curl -s -F "token=$TOKEN" -F "user=$USER" -F "text=${text}" -F "subtext=${subtext}" -F "count=$bgNow" -F "percent=${battery}" -F "title=${title}"   https://api.pushover.net/1/glances.json
+    touch $GLANCES
+  fi
 fi
+
+    if [ test cat $FILE | egrep "add'l" ]; then
+      subtext="cr ${carbsReq}g"
+    else
+      subtext="e${enactTime}"
+    fi
+    text="${bgNow}${direction}"
+    title="cob ${cob}, iob ${iob}"
+
+#    echo "pushover glance text=${text}  subtext=${subtext}  delta=${delta} title=${title}  battery percent=${battery}"
+    curl -s -F "token=$TOKEN" -F "user=$USER" -F "text=${text}" -F "subtext=${subtext}" -F "count=$bgNow" -F "percent=${battery}" -F "title=${title}"   https://api.pushover.net/1/glances.json
+    touch $GLANCES
+  fi
+fi
+    if [ test cat $FILE | egrep "add'l" ]; then
+      subtext="cr ${carbsReq}g"
+    else
+      subtext="e${enactTime}"
+    fi
+    text="${bgNow}${direction}"
+    title="cob ${cob}, iob ${iob}"
+
+#    echo "pushover glance text=${text}  subtext=${subtext}  delta=${delta} title=${title}  battery percent=${battery}"
+    curl -s -F "token=$TOKEN" -F "user=$USER" -F "text=${text}" -F "subtext=${subtext}" -F "count=$bgNow" -F "percent=${battery}" -F "title=${title}"   https://api.pushover.net/1/glances.json
+    touch $GLANCES
+  fi
+fi
+
+# Send ifttt maker event "carbs-required" if additional carbs are required
+#   and if environment variable "MAKER_KEY" is set. This is teh ifttt webhooks Maker key
+#   https://ifttt.com/maker_webhooks
+#   The "carbs-required" event can then be used with a ifttt recipe to perform an action
+#   based on carbs required. A good use case is to use the ifttt phone action to get a phone
+#   call with this event that will read out in human language the additional carbs and other
+#   vital facts. It will leave a voice mail if not answered
+
+if [[ "$MAKER_KEY" != "null" ]] && cat $FILE | egrep "add'l"; then
+  if find monitor/ -mmin -15 | grep -q ifttt-sent; then
+     echo "carbsReq=${carbsReq} but last ifttt sent less than 15 minutes ago."
+  else
+     message="Carbs required = ${carbsReq}. Glucose = $bgNow Insulin on board = $iob Carbs on board = $cob grams."
+     echo "posting message to carbs-required ... message=$message"
+
+     ifttt="monitor/ifttt.json"
+     values="{ \"value1\":\"$message\" }"
+
+     echo $values > $ifttt
+
+     curl --request POST \
+     --header 'Content-Type: application/json' \
+     -d @$ifttt  \
+     https://maker.ifttt.com/trigger/carbs-required/with/key/${key} && touch monitor/ifttt-sent
+  fi
+fi
+
