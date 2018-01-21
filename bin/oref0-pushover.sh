752441e80867af1e33fa3fc27cba1c7c46c8b7b7
Jeremy Cunningham <34543464+jpcunningh@users.noreply.github.com>
2018-01-21 16:18:11 -0600

    Pushover Priority to Urgent (#873)
    * Pushover Priority to Urgent
    
    Set Pushover Priority to urgent to ensure user sees the message.
    
    * Updated to provide pushover preferences
    
    Added preferences to allow user to configure pushover alerts' sound, priority, snooze, and only for options.

--- bin/oref0-pushover.sh
+++ bin/oref0-pushover.sh
@@ -6,6 +6,11 @@ FILE=${3-enact/smb-suggested.json}
 SOUND=${4-none}
 SNOOZE=${5-15}
 ONLYFOR=${6-carbs}
+PRIORITY=0
+RETRY=60
+EXPIRE=600
+
+PREF_FILE=preferences.json
 
 #echo "Running: $0 $TOKEN $USER $FILE $SOUND $SNOOZE"
 
@@ -14,7 +19,61 @@ if [ -z $TOKEN ] || [ -z $USER ]; then
     exit
 fi
 
+PREF_VALUE=$(cat $PREF_FILE | jq --raw-output -r .pushover_sound 2>/dev/null)
+
+if [ ! -z $PREF_VALUE ] && [ $PREF_VALUE != "null" ]; then
+    SOUND=$PREF_VALUE
+fi
+
+PREF_VALUE=$(cat $PREF_FILE | jq .pushover_snooze 2>/dev/null)
+
+if [ ! -z $PREF_VALUE ] && [ $PREF_VALUE != "null" ] && [ "$PREF_VALUE" -eq "$PREF_VALUE" ]; then
+    SNOOZE=$PREF_VALUE
+fi
+
+PREF_VALUE=$(cat $PREF_FILE | jq --raw-output -r .pushover_only 2>/dev/null)
+
+if [ ! -z $PREF_VALUE ] && [ $PREF_VALUE != "null" ]; then
+    ONLYFOR=$PREF_VALUE
+fi
+
+PREF_VALUE=$(cat $PREF_FILE | jq .pushover_priority 2>/dev/null)
+
+if [ ! -z $PREF_VALUE ] && [ $PREF_VALUE != "null" ] && [ "$PREF_VALUE" -eq "$PREF_VALUE" ]; then
+    PRIORITY=$PREF_VALUE
+fi
+
+PREF_VALUE=$(cat $PREF_FILE | jq .pushover_retry 2>/dev/null)
+
+if [ ! -z $PREF_VALUE ] && [ $PREF_VALUE != "null" ] && [ "$PREF_VALUE" -eq "$PREF_VALUE" ]; then
+    RETRY=$PREF_VALUE
+fi
+
+PREF_VALUE=$(cat $PREF_FILE | jq .pushover_expire 2>/dev/null)
+
+if [ ! -z $PREF_VALUE ] && [ $PREF_VALUE != "null" ] && [ "$PREF_VALUE" -eq "$PREF_VALUE" ]; then
+    EXPIRE=$PREF_VALUE
+fi
+
+if [ "$SOUND" = "default" ]; then
+    SOUND_OPTION=""
+else
+    SOUND_OPTION="-F sound=$SOUND"
+fi
+
+# Set priority to the default if it is an invalid value
+if ((PRIORITY < -2)) || ((PRIORITY > 2)); then
+    PRIORITY=0
+fi
+
+if ((PRIORITY == 2)); then
+    PRIORITY_OPTIONS="-F retry=$RETRY -F expire=$EXPIRE"
+else
+    PRIORITY_OPTIONS=""
+fi
+
 date
+
 if find monitor/ -mmin -$SNOOZE | grep -q pushover-sent; then
     echo "Last pushover sent less than $SNOOZE minutes ago."
 elif ! find $FILE -mmin -5 | grep -q $FILE; then
@@ -26,5 +85,6 @@ elif [[ $ONLYFOR =~ "carb" ]] && ! cat $FILE | egrep "add'l"; then
 elif [[ $ONLYFOR =~ "insulin" ]] && ! cat $FILE | egrep "maxBolus"; then
     echo "No additional insulin required."
 else
-    curl -s -F "token=$TOKEN" -F "user=$USER" -F "sound=$SOUND" -F "message=$(jq -c "{bg, tick, carbsReq, insulinReq, reason}|del(.[] | nulls)" $FILE) - $(hostname)" https://api.pushover.net/1/messages.json && touch monitor/pushover-sent
+    curl -s -F token=$TOKEN -F user=$USER $SOUND_OPTION -F priority=$PRIORITY $PRIORITY_OPTIONS -F "message=$(jq -c "{bg, tick, carbsReq, insulinReq, reason}|del(.[] | nulls)" $FILE) - $(hostname)" https://api.pushover.net/1/messages.json && touch monitor/pushover-sent
+    echo
 fi
