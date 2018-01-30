cc1166035e7f7df74dea4221c6bb2a69df171028
Scott <scottleibrand@gmail.com>
2018-01-29 21:11:08 -0800

    64-bit doesn't work: use 32-bit go for Edison

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -1037,7 +1037,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
             if uname -m | grep armv6l; then
                 cd /tmp && wget -c https://storage.googleapis.com/golang/go1.9.2.linux-armv6l.tar.gz && tar -C /usr/local -xzvf /tmp/go1.9.2.linux-armv6l.tar.gz
             elif uname -m | grep i686; then
-                cd /tmp && wget -c https://dl.google.com/go/go1.9.3.linux-amd64.tar.gz && tar -C /usr/local -xzvf /tmp/go1.9.3.linux-amd64.tar.gz
+                cd /tmp && wget -c https://dl.google.com/go/go1.9.3.linux-386.tar.gz && tar -C /usr/local -xzvf /tmp/go1.9.3.linux-386.tar.gz
             fi
 
         fi
