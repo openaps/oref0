2467cbb9f6762f0397f19fa1fd6b0e8a7c9dad59
Scott <scottleibrand@gmail.com>
2018-01-31 19:06:17 -0800

    move listen output to debug level 2

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -539,7 +539,7 @@ function wait_for_silence {
     for i in $(seq 1 800); do
         echo -n .
         # returns true if it hears pump comms, false otherwise
-        if ! listen -t $waitfor's' 2>&3 ; then
+        if ! listen -t $waitfor's' 2>&4 ; then
             echo "No interfering pump comms detected from other rigs (this is a good thing!)"
             echo -n "Continuing oref0-pump-loop at "; date
             break
