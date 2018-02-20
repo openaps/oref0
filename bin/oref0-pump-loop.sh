db16c2257129be8a41c5ee6794391fbb9bad467c
Scott <scottleibrand@gmail.com>
2018-02-19 23:06:15 -0800

    retry check_clock if needed

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -167,7 +167,7 @@ function smb_reservoir_before {
     # Refresh reservoir.json and pumphistory.json
     try_fail refresh_pumphistory_and_meal
     try_fail cp monitor/reservoir.json monitor/lastreservoir.json
-    try_fail check_clock
+    retry_fail check_clock
     echo -n "Checking pump clock: "
     (cat monitor/clock-zoned.json; echo) | tr -d '\n'
     echo -n " is within 90s of current time: " && date
