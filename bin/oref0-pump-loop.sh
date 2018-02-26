7ca26c50c67c6d7445487a662f43867477c74d9c
Scott <scottleibrand@gmail.com>
2018-02-25 18:30:08 -0800

    update_display before mmtune wait_for_silence

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -531,6 +531,7 @@ function maybe_mmtune {
         && mmtune
     else
         echo "pump_loop_completed more than 15m old; waiting for 40s silence before mmtuning"
+        update_display
         wait_for_silence 40
         mmtune
     fi
