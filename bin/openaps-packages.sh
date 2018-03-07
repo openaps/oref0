81c5873b752b1b16971b33c83d24f92d016de83f
Scott <scottleibrand@gmail.com>
2018-03-06 21:13:59 -0800

    syntax

--- bin/openaps-packages.sh
+++ bin/openaps-packages.sh
@@ -11,9 +11,9 @@ echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 apt-get install -y sudo
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y git python python-dev software-properties-common python-numpy python-pip nodejs-legacy watchdog strace tcpdump screen acpid vim locate jq lm-sensors || die "Couldn't install packages"
-if ! sudo apt-get install -y install npm; then
+if ! sudo apt-get install -y npm; then
     sudo bash -c "curl -sL https://deb.nodesource.com/setup_8.x | bash -" || die "Couldn't setup node 8"
-    sudo install -y apt-get install nodejs || die "Couldn't install nodejs"
+    sudo apt-get install -y nodejs || die "Couldn't install nodejs"
 fi
 sudo pip install -U openaps || die "Couldn't install openaps toolkit"
 sudo pip install -U openaps-contrib || die "Couldn't install openaps-contrib"
