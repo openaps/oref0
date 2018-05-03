fb6457002cfdf08a25eac4081c39c6a57f159fd6
James Babcock <jimrandomh@gmail.com>
2018-05-02 22:05:38 -0400

    Dead code removal
    Remove multiple copies of percentile() and percentRank() that were
    neither called nor exported.

--- bin/oref0-detect-sensitivity.js
+++ bin/oref0-detect-sensitivity.js
@@ -136,37 +136,3 @@ function init() {
 }
 module.exports = init;
 
-// From https://gist.github.com/IceCreamYou/6ffa1b18c4c8f6aeaad2
-// Returns the value at a given percentile in a sorted numeric array.
-// "Linear interpolation between closest ranks" method
-function percentile(arr, p) {
-    if (arr.length === 0) return 0;
-    if (typeof p !== 'number') throw new TypeError('p must be a number');
-    if (p <= 0) return arr[0];
-    if (p >= 1) return arr[arr.length - 1];
-
-    var index = arr.length * p,
-        lower = Math.floor(index),
-        upper = lower + 1,
-        weight = index % 1;
-
-    if (upper >= arr.length) return arr[lower];
-    return arr[lower] * (1 - weight) + arr[upper] * weight;
-}
-
-// Returns the percentile of the given value in a sorted numeric array.
-function percentRank(arr, v) {
-    if (typeof v !== 'number') throw new TypeError('v must be a number');
-    for (var i = 0, l = arr.length; i < l; i++) {
-        if (v <= arr[i]) {
-            while (i < l && v === arr[i]) i++;
-            if (i === 0) return 0;
-            if (v !== arr[i-1]) {
-                i += (v - arr[i-1]) / (arr[i] - arr[i-1]);
-            }
-            return i / l;
-        }
-    }
-    return 1;
-}
-
