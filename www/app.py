e8b261b92338498f53f6c94fbd525bd67e9fe452
tynbendad <tynbendad@gmail.com>
2018-05-15 13:55:59 -0600

    add sgv.json?count=# route (#1001)
    * add sgv.json?count=# route
    
    support similar to xdrip+, for offline use with watchfaces, etc.
    
    * add units_hint to sgv.json

--- www/app.py
+++ www/app.py
@@ -1,7 +1,7 @@
 import os
 import socket
 
-from flask import Flask, render_template, url_for, json, jsonify
+from flask import Flask, render_template, url_for, json, jsonify, request
 from datetime import datetime
 import pytz
 
@@ -50,6 +50,23 @@ def glucose():
         json_url = os.path.join("/root/myopenaps/monitor/glucose.json")
     data = json.load(open(json_url))
     return jsonify(data)
+
+@app.route("/sgv.json")
+def sgvjson():
+    json_url = os.path.join("/root/myopenaps/settings/profile.json")
+    data = json.load(open(json_url))
+    units = data['out_units']
+    count = request.args.get('count', default = 10, type = int)
+    if os.path.getmtime("/root/myopenaps/xdrip/glucose.json") > os.path.getmtime("/root/myopenaps/monitor/glucose.json"):
+        json_url = os.path.join("/root/myopenaps/xdrip/glucose.json")
+    else:
+        json_url = os.path.join("/root/myopenaps/monitor/glucose.json")
+    data = json.load(open(json_url))
+    if units == "mg/dL":
+        data[0]['units_hint'] = "mgdl"
+    else:
+        data[0]['units_hint'] = "mmol"
+    return jsonify(data[0:count])
 
 @app.route("/temptargets")
 def temptargets():
