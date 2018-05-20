22da32eab21b37393805d2afb9883f315db4d943
Scott <scottleibrand@gmail.com>
2018-05-20 01:20:50 -0700

    don't retry esc, to avoid multi-rig SMB race conditions

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -316,7 +316,7 @@ function smb_bolus {
     && if (grep -q '"units":' enact/smb-suggested.json 2>&3); then
         # press ESC four times on the pump to exit Bolus Wizard before SMBing, to help prevent A52 errors
         echo -n "Sending ESC ESC ESC ESC to exit any open menus before SMBing "
-        retry_return mdt -f internal button esc esc esc esc 2>&3 \
+        try_return mdt -f internal button esc esc esc esc 2>&3 \
         && echo -n "and bolusing " && jq .units enact/smb-suggested.json | tr -d '\n' && echo " units" \
         && ( try_return mdt bolus enact/smb-suggested.json 2>&3 && jq '.  + {"received": true}' enact/smb-suggested.json > enact/bolused.json ) \
         && rm -rf enact/smb-suggested.json
