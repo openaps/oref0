882593990845e2c47a7f30e15b4098990518982a
Scott <scottleibrand@gmail.com>
2018-06-07 22:33:20 -0700

    incorporate more recent crontab changes

--- bin/oref0-cron-every-minute.sh
+++ bin/oref0-cron-every-minute.sh
@@ -22,7 +22,7 @@ BT_MAC="$(get_pref_string .bt_mac "")"
 PUSHOVER_TOKEN="$(get_pref_string .pushover_token "")"
 PUSHOVER_USER="$(get_pref_string .pushover_user "")"
 
-ps aux | grep -v grep | grep -q "oref0-online '$BT_MAC'" || oref0-online '$BT_MAC' 2>&1 >> /var/log/openaps/network.log &
+ps aux | grep -v grep | grep bash | grep -q "oref0-online '$BT_MAC'" || oref0-online '$BT_MAC' 2>&1 >> /var/log/openaps/network.log &
 
 sudo wpa_cli scan &
 
@@ -30,6 +30,7 @@ sudo wpa_cli scan &
     killall -g --older-than 30m openaps
     killall -g --older-than 30m oref0-pump-loop
     killall -g --older-than 30m openaps-report
+    killall -g --older-than 10m oref0-g4-loop
 ) &
 
 # kill pump-loop after 5 minutes of not writing to pump-loop.log
@@ -39,7 +40,11 @@ if [[ ${CGM,,} =~ "g5-upload" ]]; then
     oref0-upload-entries &
 fi
 
-if [[ ${CGM,,} =~ "shareble" || ${CGM,,} =~ "g4-upload" ]]; then
+if [[ ${CGM,,} =~ "g4-go" ]]; then
+        cd $CGM_LOOPDIR
+        ps aux | grep -v grep | grep bash | grep -q 'oref0-g4-loop' || oref0-g4-loop | tee -a /var/log/openaps/cgm-loop.log
+# TODO: deprecate g4-upload and g4-local-only
+elif [[ ${CGM,,} =~ "g4-upload" ]]; then
     (
         cd $CGM_LOOPDIR
         ps aux | grep -v grep | grep -q 'openaps monitor-cgm' || (date; openaps monitor-cgm) | tee -a /var/log/openaps/cgm-loop.log
@@ -54,18 +59,18 @@ elif ! [[ ${CGM,,} =~ "mdt" ]]; then # use nightscout for cgm
     ps aux | grep -v grep | grep -q 'openaps get-bg' || ( date; openaps get-bg ; cat cgm/glucose.json | jq -r  '.[] | \"\\(.sgv) \\(.dateString)\"' | head -1 ) | tee -a /var/log/openaps/cgm-loop.log &
 fi
 
-ps aux | grep -v grep | grep -q 'oref0-ns-loop' || oref0-ns-loop | tee -a /var/log/openaps/ns-loop.log &
+ps aux | grep -v grep | grep bash | grep -q 'oref0-ns-loop' || oref0-ns-loop | tee -a /var/log/openaps/ns-loop.log &
 
-ps aux | grep -v grep | grep -q 'oref0-autosens-loop' || oref0-autosens-loop 2>&1 | tee -a /var/log/openaps/autosens-loop.log &
+ps aux | grep -v grep | grep bash | grep -q 'oref0-autosens-loop' || oref0-autosens-loop 2>&1 | tee -a /var/log/openaps/autosens-loop.log &
 
-( ps aux | grep -v grep | grep bash | grep -q 'bin/oref0-pump-loop' || oref0-pump-loop ) 2>&1 | tee -a /var/log/openaps/pump-loop.log &
+( ps aux | grep -v grep | grep bash | grep bash | grep -q 'bin/oref0-pump-loop' || oref0-pump-loop ) 2>&1 | tee -a /var/log/openaps/pump-loop.log &
 
 if [[ ! -z "$BT_PEB" ]]; then
     ( ps aux | grep -v grep | grep -q 'peb-urchin-status $BT_PEB' || peb-urchin-status $BT_PEB ) 2>&1 | tee -a /var/log/openaps/urchin-loop.log &
 fi
 
 if [[ ! -z "$BT_PEB" || ! -z "$BT_MAC" ]]; then
-    ps aux | grep -v grep | grep -q "oref0-bluetoothup" || oref0-bluetoothup >> /var/log/openaps/network.log &
+    ps aux | grep -v grep | grep bash | grep -q "oref0-bluetoothup" || oref0-bluetoothup >> /var/log/openaps/network.log &
 fi
 
 if [[ ! -z "$PUSHOVER_TOKEN" && ! -z "$PUSHOVER_USER" ]]; then
