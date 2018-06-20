59f8dccec6baf56662335ce88ba516d9fe849ac8
Scott <scottleibrand@gmail.com>
2018-06-19 19:44:10 -0700

    add support for carb entries

--- bin/oref0-simulator.sh
+++ bin/oref0-simulator.sh
@@ -22,22 +22,22 @@ function init {
 function main {
 
     jq .isfProfile profile.json > isf.json
-# only run autosens every "20m"
+    # only run autosens every "20m"
     if egrep T[0-2][0-9]:[024]0: clock.json; then
         oref0-detect-sensitivity glucose.json pumphistory.json isf.json basal_profile.json profile.json carbhistory.json > autosens.json
     fi
     oref0-calculate-iob pumphistory.json profile.json clock.json autosens.json > iob.json
-# calculate naive IOB without autosens
+    # calculate naive IOB without autosens
     oref0-calculate-iob pumphistory.json profile.json clock.json > naive_iob.json
     oref0-meal pumphistory.json profile.json clock.json glucose.json basal_profile.json carbhistory.json > meal.json
-# calculate naive BGI and deviation without autosens
+    # calculate naive BGI and deviation without autosens
     oref0-determine-basal naive_iob.json temp_basal.json glucose.json profile.json meal.json --microbolus --currentTime $(echo $(date -d $(cat clock.json | tr -d '"') +%s)000) > naive_suggested.json
     cat naive_suggested.json | jq -C -c '. | del(.predBGs) | del(.reason)'
     oref0-determine-basal iob.json temp_basal.json glucose.json profile.json autosens.json meal.json --microbolus --currentTime $(echo $(date -d $(cat clock.json | tr -d '"') +%s)000) > suggested.json
     jq . -c suggested.json >> log.json
     cat suggested.json | jq -C -c '. | del(.predBGs) | del(.reason)'
     cat suggested.json | jq -C -c .reason
-#cat suggested.json | jq -C -c .predBGs
+    #cat suggested.json | jq -C -c .predBGs
     echo -n "ZT:  " && jq -C -c .predBGs.ZT suggested.json
     echo -n "IOB: " && jq -C -c .predBGs.IOB suggested.json
     echo -n "UAM: " && jq -C -c .predBGs.UAM suggested.json
@@ -94,13 +94,28 @@ function main {
         echo "Glucose < 39 invalid"
         echo '[ { "date": '$(echo $(date -d $(cat clock.json | tr -d '"')+5minutes +%s)000)', "glucose": 39, "sgv": 39, "dateString": "'$(date -d $(cat clock.json | tr -d '"')+5minutes -Iseconds )'", "device": "fakecgm" } ] ' | tee newrecord.json
     fi
-# write a new glucose entry to glucose.json, and truncate it to 432 records (36 hours)
+    # write a new glucose entry to glucose.json, and truncate it to 432 records (36 hours)
     jq -s '[.[][]] | .[0:432]' newrecord.json glucose.json > glucose.json.new
     mv glucose.json.new glucose.json
 
-# advance the clock by 5m
+    # if there are any new carbs, add them to carbhistory.json
+    addcarbs $carbs
+
+    # advance the clock by 5m
     if jq -e .deliverAt suggested.json >/dev/null; then
         echo '"'$(date -d "$(cat suggested.json | jq .deliverAt | tr -d '"')+5 minutes" -Iseconds)'"' > clock.json
+    fi
+}
+
+function addcarbs {
+    # if a carbs argument is provided, write the carb entry to carbhistory.json
+    carbs=$1
+    if ! [ -z "$carbs" ] && [ "$carbs" -gt 0 ]; then
+        echo '[ { "carbs": '$carbs', "insulin": null, "created_at": "'$(date -d $(cat clock.json | tr -d '"')+5minutes -Iseconds )'", "enteredBy": "oref0-simulator" } ] ' | tee newrecord.json
+
+        # write the new record to carbhistory.json, and truncate it to 100 records
+        jq -s '[.[][]] | .[0:100]' newrecord.json carbhistory.json > carbhistory.json.new
+        mv carbhistory.json.new carbhistory.json
     fi
 }
 
@@ -109,6 +124,8 @@ if [[ $1 == *"init"* ]]; then
 else
     deviation=$1
     noise=$2
+    carbs=$3
+    echo Running oref-simulator with deviation $deviation, noise $noise, and carbs $carbs
     main
 fi
 
