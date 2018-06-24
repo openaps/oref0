ce9a48f2d538fd5f5f01415c4c0c331e59ef88a4
jaylagorio <jay@lagor.io>
2018-06-23 22:28:50 -0400

    Update openaps-packages.sh

--- bin/openaps-packages.sh
+++ bin/openaps-packages.sh
@@ -15,7 +15,9 @@ echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 apt-get install -y sudo
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y git python python-dev software-properties-common python-numpy python-pip nodejs-legacy watchdog strace tcpdump screen acpid vim locate jq lm-sensors || die "Couldn't install packages"
-if ! sudo apt-get install -y npm; then
+#if ! sudo apt-get install -y npm; then
+# install/upgrade to node 8
+if ! nodejs --version | grep 'v8.'; then
     sudo bash -c "curl -sL https://deb.nodesource.com/setup_8.x | bash -" || die "Couldn't setup node 8"
     sudo apt-get install -y nodejs || die "Couldn't install nodejs"
 fi
@@ -26,5 +28,3 @@ sudo activate-global-python-argcomplete || die "Couldn't run activate-global-pyt
 sudo npm install -g json oref0 || die "Couldn't install json and oref0"
 echo openaps installed
 openaps --version
-
-
