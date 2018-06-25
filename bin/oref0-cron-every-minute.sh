de462defb4d0c9a8fa6b173cf29febe2e6a165ca
Scott <scottleibrand@gmail.com>
2018-06-25 15:05:02 -0700

    re-add oref0-radio-reboot

--- bin/oref0-cron-every-minute.sh
+++ bin/oref0-cron-every-minute.sh
@@ -60,6 +60,9 @@ find /var/log/openaps/pump-loop.log -mmin +5 | grep pump && (
     killall -g --older-than 5m openaps-report
 ) | tee -a /var/log/openaps/pump-loop.log &
 
+# if the rig doesn't recover after that, reboot:
+oref0-radio-reboot
+
 if [[ ${CGM,,} =~ "g5-upload" ]]; then
     oref0-upload-entries &
 fi
