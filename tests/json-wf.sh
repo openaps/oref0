821de29a8e46970233e71e044cee5fa5a4f328ed
Dave Carlson <thecubic@thecubic.net>
2018-06-25 08:57:18 -0700

    add a well-formedness checker for JSON

---
+++ tests/json-wf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# tests for json well-formedness by throwing each file through
+# jq, reporting state to the terminal with color and producing
+# an end return code suitable for CI
+
+rc=0
+
+myloc=$(dirname ${0})
+
+cd ${myloc}/..
+
+red='\033[1;31m'
+green='\033[1;32m'
+nocolor='\033[0m'
+
+for jsonfile in $(find . -name '*.json'); do
+  realfile=$(realpath ${jsonfile})
+  jq . ${jsonfile} >/dev/null 2>/dev/null
+  ret=$?
+  # accumulate failures
+  rc=$((rc|ret))
+  if [ ${ret} -eq 0 ]; then
+    echo -e "${green}${realfile}${nocolor}"
+  else
+    echo -e "${red}${realfile} ${rc}${nocolor}"
+  fi
+done
+
+exit ${rc}  
