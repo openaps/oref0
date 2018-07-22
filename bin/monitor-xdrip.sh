ae7fd5ba26e447dcd914a9f2f7b2ce6dff0b5eb8
Andy Sharrow <dramageek@gmail.com>
2018-07-21 22:44:53 -0400

    Update to pull glucose from xDrip+
    Uses xDrip+ webserver on the phone rather than xdripAPS

--- bin/monitor-xdrip.sh
+++ bin/monitor-xdrip.sh
@@ -9,10 +9,11 @@ EOT
 
 date
 cp -rf xdrip/glucose.json xdrip/last-glucose.json
-curl --compressed -s http://localhost:5000/api/v1/entries?count=288 | json -e "this.glucose = this.sgv" > xdrip/glucose.json
+curl --compressed -s --header "api-secret: "$API_SECRET http://192.168.44.1:17580/sgv.json?count=288 | json -e "this.glucose = this.sgv" > xdrip/glucose.json
 if ! cmp --silent xdrip/glucose.json xdrip/last-glucose.json; then
     if cat xdrip/glucose.json | json -c "minAgo=(new Date()-new Date(this.dateString))/60/1000; return minAgo < 10 && minAgo > -5 && this.glucose > 38" | grep -q glucose; then
         cp -up xdrip/glucose.json monitor/glucose.json
+        echo "Updating glucose.json from xDrip+"
     else
         echo No recent glucose data downloaded from xDrip.
         diff xdrip/glucose.json xdrip/last-glucose.json
