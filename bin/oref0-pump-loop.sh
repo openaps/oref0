f7b53ee394e0774eb1c60035a995ae1969399564
Scott <scottleibrand@gmail.com>
2018-08-23 09:57:22 -0700

    re-create monitor and settings directories if needed

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -571,6 +571,9 @@ function mdt_get_bg {
 # make sure we can talk to the pump and get a valid model number
 function preflight {
     echo -n "Preflight "
+    # re-create directories if they got manually deleted
+    mkdir -p settings
+    mkdir -p monitor
     # only 515, 522, 523, 715, 722, 723, 554, and 754 pump models have been tested with SMB
     ( check_model || check_model ) 2>&3 >&4 \
     && ( egrep -q "[57](15|22|23|54)" settings/model.json || (grep -q 12 settings/model.json && echo -n "(x12 models do not support SMB safety checks, SMB will not be available.) ") ) \
