d07e82489128db3fdabdfbe9da2e0c31b2dc1dd9
Alim hassam <alimhassam@gmail.com>
2018-08-29 23:26:17 -0400

    Add one more esc

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -425,7 +425,7 @@ function smb_bolus {
         echo -n "Sending ESC ESC ESC ESC to exit any open menus before SMBing "
         try_return mdt -f internal button esc 2>&3 \
         && sleep 0.5s \  
-        && try_return mdt -f internal button esc esc esc 2>&3 \
+        && try_return mdt -f internal button esc esc esc esc 2>&3 \
         && echo -n "and bolusing " && jq .units enact/smb-suggested.json | nonl && echo " units" \
         && ( try_return mdt bolus enact/smb-suggested.json 2>&3 && jq '.  + {"received": true}' enact/smb-suggested.json > enact/bolused.json ) \
         && rm -rf enact/smb-suggested.json
