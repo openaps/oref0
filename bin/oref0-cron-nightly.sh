52f08a6fa8811f8064f7b7b2bc5b2519c9758f08
Scott <scottleibrand@gmail.com>
2019-02-09 17:02:18 -0800

    Revert "use --categorize-uam-as-basal=true for now"
    This reverts commit b8bffc6b350a00a9bf483f44ca9b66fde785a4bd.

--- bin/oref0-cron-nightly.sh
+++ bin/oref0-cron-nightly.sh
@@ -18,5 +18,5 @@ directory="$PWD"
 if [[ $ENABLE =~ autotune ]]; then
     # autotune nightly at 4:05am using data from NS
     echo "=== Running Autotune at `date` === " | tee -a /var/log/openaps/autotune.log
-    (oref0-autotune -d=$directory -n=$NIGHTSCOUT_HOST --categorize-uam-as-basal=true && cat $directory/autotune/profile.json | jq . | grep -q start && cp $directory/autotune/profile.json $directory/settings/autotune.json) 2>&1 | tee -a /var/log/openaps/autotune.log &
+    (oref0-autotune -d=$directory -n=$NIGHTSCOUT_HOST && cat $directory/autotune/profile.json | jq . | grep -q start && cp $directory/autotune/profile.json $directory/settings/autotune.json) 2>&1 | tee -a /var/log/openaps/autotune.log &
 fi
