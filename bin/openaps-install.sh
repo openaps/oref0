2ce4acb9efd335db4eae8e774bfa7dcbb0f19b04
Jon Cluck <cluckj@users.noreply.github.com>
2019-03-30 22:48:34 -0400

    Workarounds for Jubilinux v0.2.0 (Debian Jessie)

--- bin/openaps-install.sh
+++ bin/openaps-install.sh
@@ -25,6 +25,16 @@ grep "PermitRootLogin yes" /etc/ssh/sshd_config || echo "PermitRootLogin yes" > 
 # set timezone
 dpkg-reconfigure tzdata
 
+#Workarounds for Jubilinux v0.2.0 (Debian Jessie) migration to LTS
+if cat /etc/os-release | grep 'PRETTY_NAME="Debian GNU/Linux 8 (jessie)"' &> /dev/null; then
+    #Disable validity check for archived Debian repos
+    echo "Acquire::Check-Valid-Until false;" | tee -a /etc/apt/apt.conf.d/10-nocheckvalid
+    #Replace apt sources.list with new archive.debian.org locations
+    echo -e "deb http://security.debian.org/ jessie/updates main\n#deb-src http://security.debian.org/ jessie/updates main\n\ndeb http://archive.debian.org/debian/ jessie-backports main\n#deb-src http://archive.debian.org/debian/ jessie-backports main\n\ndeb http://archive.debian.org/debian/ jessie main contrib non-free\n#deb-src http://archive.debian.org/debian/ jessie main contrib non-free" > /etc/apt/sources.list
+    #Use nodesource setup script to Add nodesource repository to sources.list.d
+    curl -sL https://deb.nodesource.com/setup_8.x | bash -
+fi
+
 #dpkg -P nodejs nodejs-dev
 # TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
 apt-get -o Acquire::ForceIPv4=true update && apt-get -o Acquire::ForceIPv4=true -y dist-upgrade && apt-get -o Acquire::ForceIPv4=true -y autoremove
