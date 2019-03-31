edc94de7bcf5997d10304bfa4e493b758a4a29e9
Jon Cluck <cluckj@users.noreply.github.com>
2019-03-30 22:49:40 -0400

    Workarounds for Jubilinux v0.2.0 (Debian Jessie)

--- bin/openaps-packages.sh
+++ bin/openaps-packages.sh
@@ -3,8 +3,7 @@
 # TODO: remove the `-o Acquire::ForceIPv4=true` once Debian's mirrors work reliably over IPv6
 apt-get -o Acquire::ForceIPv4=true install -y sudo
 sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::ForceIPv4=true -y upgrade
-sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev software-properties-common python-numpy python-pip npm watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
-if getent passwd edison > /dev/null; then sudo apt-get -o Acquire::ForceIPv4=true install -y nodejs-legacy; fi && \
+sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev software-properties-common python-numpy python-pip nodejs watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
 sudo pip install -U openaps && \
 sudo pip install -U openaps-contrib && \
 sudo openaps-install-udev-rules && \
