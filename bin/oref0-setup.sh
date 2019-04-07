9c0a3b39ae2f622952814712081ecf1e9c013c05
Jon Cluck <cluckj@users.noreply.github.com>
2019-04-07 10:47:23 -0400

    Install master branch of MRAA
    The 1.7.0 branch no longer exists

--- bin/oref0-setup.sh
+++ bin/oref0-setup.sh
@@ -816,9 +816,9 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
             if ! ldconfig -p | grep -q mraa; then # if not installed, install it
                 echo Installing swig etc.
                 sudo apt-get install -y libpcre3-dev git cmake python-dev swig || die "Could not install swig etc."
-                # TODO: Due to mraa bug https://github.com/intel-iot-devkit/mraa/issues/771 we were not using the master branch of mraa on dev.
                 # TODO: After each oref0 release, check whether there is a new stable MRAA release that is of interest for the OpenAPS community
-                MRAA_RELEASE="v1.7.0" # GitHub hash 8ddbcde84e2d146bc0f9e38504d6c89c14291480
+                # Latest (as of April 2019) master branch of mraa is working
+                MRAA_RELEASE="master" # GitHub hash 8ddbcde84e2d146bc0f9e38504d6c89c14291480
                 if [ -d "$HOME/src/mraa/" ]; then
                     echo -n "$HOME/src/mraa/ already exists; "
                     #(echo "Pulling latest master branch" && cd ~/src/mraa && git fetch && git checkout master && git pull) || die "Couldn't pull latest mraa master" # used for oref0 dev
