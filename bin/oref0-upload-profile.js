75d08e1528bedae0529cc41b2a8f921eacc4eb6c
Dana Lewis <danamlewis@users.noreply.github.com>
2019-10-07 18:49:11 -0700

    Have autotune sync to NS nightly after autotune runs (#1098)
    * Have autotune sync to NS nightly after autotune runs
    
    This will keep Nightscout in sync with autotune after the nightly run.
    
    * always round insulin to carb ratios to 0.1g
    
    * round basal rates to 0.01 U/h, not 0.001
    
    * sleep 60s to generate a new profile.json

--- bin/oref0-upload-profile.js
+++ bin/oref0-upload-profile.js
@@ -120,12 +120,13 @@ if (!module.parent) {
         // Basals
 
         var new_basal = [];
+        var decimals = 100; // always round basal rates to 0.01 U/h
 
         _.forEach(profiledata.basalprofile, function(basalentry) {
 
             var newEntry = {
                 time: '' + basalentry.start.substring(0, 5)
-                , value: '' + +(Math.round(basalentry.rate + 'e+3') + 'e-3')
+                , value: '' + Math.round( basalentry.rate  * decimals) / decimals
                 , timeAsSeconds: '' + basalentry.minutes * 60
             };
 
@@ -220,12 +221,13 @@ if (!module.parent) {
         // Carb ratios
 
         var new_carb_ratios = [];
+        var decimals = 10; // always round insulin to carb ratios to 0.1g
 
         _.forEach(profiledata.carb_ratios.schedule, function(carb_entry) {
 
             var new_entry = {
                 time: carb_entry.start.substring(0, 5)
-                , value: '' + carb_entry.ratio
+                , value: '' + Math.round( carb_entry.ratio * decimals) / decimals
                 , timeAsSeconds: '' + carb_entry.offset * 60
             };
 
