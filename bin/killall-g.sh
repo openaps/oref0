8d3a2e3bc60f3ac9753e40fc2dc2866160c095f8
Scott <scottleibrand@gmail.com>
2019-10-25 20:43:16 -0700

    expand using tzachi's suggested code

--- bin/killall-g.sh
+++ bin/killall-g.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env bash
 
 # Usage: killall-g command [seconds]
-# Kill the named command and its entire process group, like "killall -g command"
+# Kill the named bash script and its entire process group, like "killall -g command"
 # if a second argument is provided, works much like killall -g --older-than,
 # and looks for processes older than the specified number of seconds
-# works will bash scripts where killall doesn't (it only lets you killall running instances of bash)
-# only  kills a single process group, the one with the highest PGID
+# works with bash scripts called via "bash script_name", which killall doesn't support
+# (it only lets you killall based on the process name, which is just "bash"
+# for non-bash scripts, you can specify a third argument to match against fname
 
+script=$1
 if [[ -z "$2" ]]; then
-    elapsed=0
+    older_than=0
 else
-    elapsed=$2
+    older_than=$2
 fi
-ps x -O pgid,etimes | egrep -v "grep|killall" | grep $1 | tail -1 | awk '{if ($3 >= '$elapsed') print $2}'  | while read pgid; do kill -- -$pgid; done
+if [[ -z "$3" ]]; then
+    process="bash"
+else
+    process=$3
+fi
+ps x -A -o pid,fname,etimes,pgid,args |   while read pid fname etimes pgid args; do
+    #echo $pid
+    if [[ $fname == $process ]] && [[ $args == $script ]] &&  [[ $etimes > $older_than ]]; then
+        echo killing all $fname $args
+        kill -- -$pgid
+    fi;
+done;
+#ps x -O pgid,etimes | egrep -v "grep|killall" | grep $1 | tail -1 | awk '{if ($3 >= '$older_than') print $2}'  | while read pgid; do kill -- -$pgid; done
