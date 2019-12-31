e3dd0cb35e9c7528f20f8c4f2f64b7d245d67814
Scott Leibrand <scottleibrand@gmail.com>
2019-12-31 13:32:17 -0800

    deprecate/remove support for max_bg and wide_bg_target_range

--- tests/command-behavior.tests.sh
+++ tests/command-behavior.tests.sh
@@ -570,8 +570,7 @@ EOT
    "sensitivity_raises_target": true,
    "suspend_zeros_iob": true,
    "unsuspend_if_no_temp": false,
-   "useCustomPeakTime": true,
-   "wide_bg_target_range": false
+   "useCustomPeakTime": true
 }
 EOT
 
@@ -675,8 +674,7 @@ EOT
    "sensitivity_raises_target": true,
    "suspend_zeros_iob": true,
    "unsuspend_if_no_temp": false,
-   "useCustomPeakTime": true,
-   "wide_bg_target_range": false
+   "useCustomPeakTime": true
 }
 EOT
 
