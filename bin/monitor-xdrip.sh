d52378c63e925f4afad151acdacf9f5c5745edde
Jordy Ruiz <me@jruiz.fr>
2019-12-31 16:45:18 +0100

    Replace the same json call in monitor-xdrip by jq

--- bin/monitor-xdrip.sh
+++ bin/monitor-xdrip.sh
@@ -11,7 +11,12 @@ date
 cp -rf xdrip/glucose.json xdrip/last-glucose.json
 curl --compressed -s http://localhost:5000/api/v1/entries?count=288 | jq '[ .[] | .glucose = .sgv ]' > xdrip/glucose.json
 if ! cmp --silent xdrip/glucose.json xdrip/last-glucose.json; then
-    if cat xdrip/glucose.json | json -c "minAgo=(new Date()-new Date(this.dateString))/60/1000; return minAgo < 10 && minAgo > -5 && this.glucose > 38" | grep -q glucose; then
+    if cat xdrip/glucose.json
+    	|  jq \
+	        --arg AT_LATEST   "`date -u +'%Y-%m-%dT%H:%M:%S.000Z' --date '5 minutes'`"\
+	        --arg AT_EARLIEST "`date -u +'%Y-%m-%dT%H:%M:%S.000Z' --date '-10 minutes'`"\
+	        '[.[] | (select (.dateString >= $AT_EARLIEST)) | (select (.dateString <= $AT_LATEST)) | (select (.glucose > 38))]'
+    	| grep -q glucose; then
         cp -up xdrip/glucose.json monitor/glucose.json
     else
         echo No recent glucose data downloaded from xDrip.
