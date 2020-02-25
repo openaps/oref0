e1573c6abc1138d64a936794aeb75c1ce728c704
Scott <scottleibrand@gmail.com>
2020-02-25 08:34:01 -0800

    pull a complete pumphistory instead of just 24h as a workaround for a page parsing bug

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -835,7 +835,7 @@ function compare_with_fullhistory() {
   set -o pipefail
   rm monitor/full-pumphistory-24h-zoned.json
   echo -n "Full history for testing refresh" \
-  && ((( pumphistory -n 27 2>&3 | jq -f openaps.jq 2>&3 | tee monitor/full-pumphistory-24h-zoned.json 2>&3 >&4 ) \
+  && ((( pumphistory -a 2>&3 | jq -f openaps.jq 2>&3 | tee monitor/full-pumphistory-24h-zoned.json 2>&3 >&4 ) \
       && echo -n ed) \
      || (echo " failed. "; return 1)) \
   && echo " through $(jq -r '.[0].timestamp' monitor/full-pumphistory-24h-zoned.json)"
@@ -878,7 +878,7 @@ function read_full_pumphistory() {
   set -o pipefail
   rm monitor/pumphistory-24h-zoned.json
   echo -n "Full history refresh" \
-  && ((( pumphistory -n 27 2>&3 | jq -f openaps.jq 2>&3 | tee monitor/pumphistory-24h-zoned.json 2>&3 >&4 ) \
+  && ((( pumphistory -a 2>&3 | jq -f openaps.jq 2>&3 | tee monitor/pumphistory-24h-zoned.json 2>&3 >&4 ) \
       && echo -n ed) \
      || (
         echo " failed. "
