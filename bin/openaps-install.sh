59e24a39eb9eb8644b8e6350958a514d90a176dd
sharbi <seanharbison001@gmail.com>
2022-01-19 16:23:46 +1300

    Update openaps-install.sh

--- bin/openaps-install.sh
+++ bin/openaps-install.sh
@@ -58,7 +58,7 @@ apt-get -o Acquire::ForceIPv4=true update && apt-get -o Acquire::ForceIPv4=true 
 
 # Install nvm and update to node 15.14.0
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-src ~/.bashrc
+source ~/.bashrc
 nvm install 15.14.0
 
 grep "PermitRootLogin yes" /etc/ssh/sshd_config || echo "PermitRootLogin yes" > /etc/ssh/sshd_config
