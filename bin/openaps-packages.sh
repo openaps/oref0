f35b4ffc1c0b52a1b650a752352b2d58a979beb6
Dave Acklam <dcacklam@gmail.com>
2022-06-18 19:29:34 -0700

    Support for Debian Bullseye and aarch64 (Raspberry Pi OS/Bullseye 64 Bit) in setup routines (#1429)
    * Remove redundant pip install for bullseye support
    
    pIp is also installed by openaps-packages.sh
    
    * Change python default to python2, install pip, pip install numpy for bullseye support
    
    * Bullseye support on arm64 (Raspberry Pi OS 64bit)
    
    Handle aarch64 architecture properly when downloading golang binaries. 
    Without this setup will fail on Rpi 3/4/Zero2 devices where the user has installed the recently-released 64bit OS.
    
    Co-authored-by: Dave Acklam <dave.acklam@zonarsystems.com>

--- bin/openaps-packages.sh
+++ bin/openaps-packages.sh
@@ -10,7 +10,16 @@ echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 
 apt-get install -y sudo
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get install -y git python python-dev software-properties-common python-numpy python-pip watchdog strace tcpdump screen acpid vim locate lm-sensors || die "Couldn't install packages"
+## Debian Bullseye (Raspberry Pi OS 64bit, etc) is python3 by default and does not support python2-pip.
+if ! cat /etc/os-release | grep bullseye >& /dev/null; then
+   sudo apt-get install -y git python python-dev software-properties-common python-numpy python-pip watchdog strace tcpdump screen acpid vim locate lm-sensors || die "Couldn't install packages"
+else
+   # Bullseye based OS. Get PIP2 from pypa and pip-install python packages rather than using the py3 ones from apt
+   # Also, install python-is-python2, to override the distro default of linking python to python3
+   sudo apt-get install -y git python-is-python2 python-dev-is-python2 software-properties-common watchdog strace tcpdump screen acpid vim locate lm-sensors || die "Couldn't install packages"
+   curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2 || die "Couldn't install pip"
+   python2 -m pip install numpy || die "Couldn't pip install numpy"
+fi
 
 # We require jq >= 1.5 for --slurpfile for merging preferences. Debian Jessie ships with 1.4.
 if cat /etc/os-release | grep 'PRETTY_NAME="Debian GNU/Linux 8 (jessie)"' &> /dev/null; then
