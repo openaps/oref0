7d464adf218f8f3e6013d72e40b5ab7819170949
Scott <scottleibrand@gmail.com>
2022-10-13 20:10:13 -0700

    wait_for_silence  before retrying preflight

--- bin/oref0-pump-loop.sh
+++ bin/oref0-pump-loop.sh
@@ -169,7 +169,8 @@ function fail {
         refresh_after_bolus_or_enact
         echo "Incomplete oref0-pump-loop (pump suspended) at $(date)"
     else
-        # retry preflight; if successful, refresh pumphistory, else mmtune
+        # wait upto45s and retry preflight; if successful, refresh pumphistory, else mmtune
+        wait_for_silence $upto45s
         if retry_fail preflight; then
             pumphistory_daily_refresh
         else
